e3e03cdf59fcc24e1c07ce49990116e703432dea
mydearxym <mydearxym@qq.com>
2017-09-01 15:09:19 +0800

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,20 @@ import Page from '../components/Page'
 import { initStore } from '../stores/store'
 
 import { Provider } from 'mobx-react'
+import { defineMessages, FormattedMessage, IntlProvider } from 'react-intl'
+
+const { description } = defineMessages({
+  description: {
+    id: 'page.index.title',
+    defaultMessage: 'i am index fuckig header',
+  },
+})
 
 export default class Index extends React.Component {
   static getInitialProps({ req }) {
     const isServer = !!req
     const store = initStore(isServer)
+    // todo: get locale
     return { isServer, lastUpdate: store.lastUpdate }
   }
 
@@ -19,9 +28,19 @@ export default class Index extends React.Component {
   }
 
   render() {
+    const locale = 'en'
+    const messages = {}
+
     return (
       <Provider store={this.store}>
-        <Page title="Index Page" linkTo="/other" />
+        <IntlProvider locale={locale} messages={messages}>
+          <div>
+            <h1>
+              <FormattedMessage {...description} />
+            </h1>
+            <Page title="Index Page" linkTo="/other" />
+          </div>
+        </IntlProvider>
       </Provider>
     )
   }
