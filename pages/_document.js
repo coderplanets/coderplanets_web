638a8e84d43325332db98c3183aadb2cab1b84f1
mydearxym <mydearxym@qq.com>
2017-09-10 16:16:05 +0800

    support styled-components SSR

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+/* eslint-disable */
+
+export default class MyDocument extends Document {
+  render() {
+    const sheet = new ServerStyleSheet()
+    const main = sheet.collectStyles(<Main />)
+    const styleTags = sheet.getStyleElement()
+    return (
+      <html>
+        <Head>
+          <title>mastani</title>
+          {styleTags}
+        </Head>
+        <body>
+          <div className="root">{main}</div>
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
