33a4ce30da277a6b2237002bf82d31babdb04441
mydearxym <mydearxym@qq.com>
2017-09-18 13:48:23 +0800

    add locale query && locale catch

--- server.js
+++ server.js
@@ -5,31 +5,66 @@ const { parse } = require('url')
 const next = require('next')
 const mobxReact = require('mobx-react')
 const pathMatch = require('path-match')
+const { basename } = require('path')
+const accepts = require('accepts')
+const glob = require('glob')
 
 const app = next({ dev })
 const handle = app.getRequestHandler()
 const route = pathMatch()
 
 // const langMatch = route('/lang/:name')
-const homeMatch = route('/home/:name')
-
 mobxReact.useStaticRendering(true)
+
+const supportLanguages = glob
+  .sync('./lang/*.json')
+  .map(f => basename(f, '.json'))
+
+const messageCache = new Map()
+const getMessages = locale => {
+  if (!messageCache.has(locale)) {
+    /* eslint-disable import/no-dynamic-require */
+    /* eslint-disable global-require */
+    let langData = {}
+
+    try {
+      langData = require(`./lang/${locale}.json`)
+      messageCache.set(locale, langData)
+    } catch (e) {
+      return { error: 'this lang is not supported' }
+    }
+  }
+  return messageCache.get(locale)
+}
+
+const homeQuery = route('/home/:name')
+const localeQuery = route('/locale/:lang')
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    /* const parsedUrl = parse(req.url, true) */
-    /* handle(req, res, parsedUrl) */
-
     const { pathname } = parse(req.url)
-    const params = homeMatch(pathname)
+    const homeMatch = homeQuery(pathname)
+    const localeMatch = localeQuery(pathname)
 
-    if (params === false) {
-      //    console.log('not match')
-      handle(req, res)
-      return
+    const accept = accepts(req)
+    const locale = accept.language(supportLanguages) // 'zh'
+
+    if (localeMatch) {
+      console.log('localeMatch: ', localeMatch)
+      res.setHeader('Content-Type', 'application/json;charset=utf-8')
+      // TODO cache
+      return res.end(JSON.stringify(getMessages(localeMatch.lang)))
     }
-    // if match go this way
-    app.render(req, res, '/', params)
+
+    req.locale = locale
+    req.messages = getMessages(locale)
+
+    if (homeMatch) {
+      // console.log('homeMatch: ', homeMatch)
+      return app.render(req, res, '/', homeMatch)
+    }
+    // now index page go this way
+    return handle(req, res)
   }).listen(3000, err => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
