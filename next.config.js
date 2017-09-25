fa14ac4b5701db2792486eb21d3e2584c8662efa
mydearxym <mydearxym@qq.com>
2017-09-25 22:54:45 +0800

    test Preact  shrink build size 828 KB(before) to 704 KB(after)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
-exports.exportPathMap = () => ({
-  '/': { page: '/' },
-  '/home/i': { page: '/', query: { name: 'index' } },
-  '/home/feature': { page: '/', query: { name: 'feature' } },
-  '/home/theme': { page: '/', query: { name: 'theme' } },
-  '/home/i18n': { page: '/', query: { name: 'i18n' } },
-  '/home/example': { page: '/', query: { name: 'example' } },
-  '/shop': { page: '/shop' },
-})
+module.exports = {
+  exportPathMap: () => ({
+    '/': { page: '/' },
+    '/home/i': { page: '/', query: { name: 'index' } },
+    '/home/feature': { page: '/', query: { name: 'feature' } },
+    '/home/theme': { page: '/', query: { name: 'theme' } },
+    '/home/i18n': { page: '/', query: { name: 'i18n' } },
+    '/home/example': { page: '/', query: { name: 'example' } },
+    '/shop': { page: '/shop' },
+  }),
+
+  webpack: (config, { dev }) => {
+    // For the development version, we'll use React.
+    // Because, it supports react hot loading and so on.
+    if (dev) {
+      return config
+    }
+
+    config.resolve.alias = {
+      react: 'preact-compat/dist/preact-compat',
+      'react-dom': 'preact-compat/dist/preact-compat',
+    }
+
+    return config
+  },
+}
