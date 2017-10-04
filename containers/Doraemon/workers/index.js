4de0d45fc6f407dcfaa8dafe8a24293dfd239c1b
mydearxym <mydearxym@qq.com>
2017-10-05 00:02:57 +0800

    wip: refactor some pocket logic to works

---
+++ containers/Doraemon/workers/index.js
@@ -0,0 +1,53 @@
+import R from 'ramda'
+import { Observable } from 'rxjs/Observable'
+// import fetch from 'isomorphic-fetch'
+
+import { pl, framework, cmd } from '../suggestions'
+// import { makeDebugger } from '../../../utils/debug'
+
+// const debug = makeDebugger('L:Doraemon:worker')
+
+const ALL_SUGGESTIONS = R.mergeAll([pl, framework, cmd])
+const lowerStartWith = R.compose(R.startsWith, R.toLower)
+const LowerKeys = R.keys(ALL_SUGGESTIONS).map(R.toLower)
+
+const suggestionStartWith = (val, ...source) =>
+  R.filter(lowerStartWith(val), source)
+
+const stripInput = R.ifElse(R.startsWith('/'), R.slice(1, Infinity), R.identity)
+
+const getRelatedOptions = R.compose(
+  R.partialRight(suggestionStartWith, LowerKeys),
+  stripInput
+)
+
+const getSuggestionPromise = query => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      return resolve(getRelatedOptions(query))
+    })
+  })
+}
+
+const formater = name => ({
+  title: name,
+  desc: '帖子: 233, 用户: 344',
+})
+
+const formatSuggestion = R.map(formater)
+
+export const getSuggestions$ = query => {
+  const promise = getSuggestionPromise(query)
+  return Observable.fromPromise(promise).map(formatSuggestion)
+}
+
+export const fuck = 1
+
+/*
+   const hasValueExceptSlash = R.compose(R.lte(2), R.length)
+   const slashAndNotEmpty = R.allPass([
+   R.startsWith('/'),
+   isNotEmptyValue,
+   hasValueExceptSlash,
+   ])
+ */
