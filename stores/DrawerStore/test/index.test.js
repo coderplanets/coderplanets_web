868245b83e8da01233b0e7e4c63471822ffd9751
mydearxym <mydearxym@qq.com>
2017-10-21 14:49:18 +0800

    warning fix

--- stores/DrawerStore/test/index.test.js
+++ stores/DrawerStore/test/index.test.js
@@ -2,11 +2,8 @@
 * DrawerStore store test
 *
 */
+// import DrawerStore from '../index'
 
-
-import DrawerStore from '../index'
-
-
-it("1 + 1 = 2", () => {
-  expect(1+1).toBe(2)
+it('1 + 1 = 2', () => {
+  expect(1 + 1).toBe(2)
 })
