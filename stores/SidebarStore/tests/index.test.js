e5e02f011ce37485bab420acdc719f05be14be40
mydearxym <mydearxym@qq.com>
2017-10-24 11:25:33 +0800

    fix sidebarStore test

--- stores/SidebarStore/tests/index.test.js
+++ stores/SidebarStore/tests/index.test.js
@@ -3,13 +3,21 @@
 *
 */
 
+import R from 'ramda'
+
+import RootStore from '../../RootStore'
 import SidebarStore from '../index'
 
-it('sidebar store create', () => {
-  const sidebar = SidebarStore.create({}, {})
+it('sidebar store create, init menuItems = 0', () => {
+  const sidebar = SidebarStore.create({})
+  const menuItems = sidebar.menuItems.toJSON()
 
-  expect(sidebar.menuItemsData.length).toBe(0)
+  expect(menuItems.length).toBe(0)
+})
 
-  sidebar.loadMenuItem()
-  expect(sidebar.menuItemsData.length).toBe(6)
+it('after rootStore create, sidebar menuItems should = communities count', () => {
+  const rootStore = RootStore.create({ appLangs: {} })
+
+  const communitiesCount = R.length(R.values(rootStore.communities.all))
+  expect(rootStore.sidebar.menuItems.length).toBe(communitiesCount)
 })
