7c01d752bd76fce5ebb7ff85c4ebe7ae0fd6e0aa
mydearxym <mydearxym@qq.com>
2017-10-26 17:23:39 +0800

    explore antd

--- components/Button/styles/index.js
+++ components/Button/styles/index.js
@@ -1,7 +1,30 @@
-import styled from 'styled-components'
+import styled, { injectGlobal } from 'styled-components'
 
 import { Button } from 'antd'
 
-const MButton = styled(Button)`color: tomato;`
+const MButton = styled(Button)`
+  color: tomato;
+  &:hover {
+    color: tomato;
+    border-color: tomato;
+  }
+  &:focus {
+    color: tomato;
+    border-color: tomato;
+  }
+  &:active {
+    color: tomato;
+    border-color: tomato;
+  }
+`
+
+/* eslint-disable no-unused-expressions */
+// TODO: move to global
+injectGlobal`
+ .ant-btn-clicked:after {
+  border: 0 solid tomato;
+ }
+`
+/* eslint-enable no-unused-expressions */
 
 export default MButton
