c3a40e1aa115bd0a1756ee1e23587c8f09adc475
mydearxym <mydearxym@qq.com>
2018-04-16 15:48:57 +0800

    communities wip

--- containers/CommunitiesContent/index.js
+++ containers/CommunitiesContent/index.js
@@ -1,18 +1,99 @@
 /*
-*
-* CommunitiesContent
-*
-*/
+ *
+ * CommunitiesContent
+ *
+ */
 
 import React from 'react'
 import { inject, observer } from 'mobx-react'
+import Trend from 'react-trend'
+import { Button, Icon } from 'antd'
 
 // import Link from 'next/link'
 
-import { makeDebugger, storeSelector } from '../../utils'
+import { makeDebugger, storeSelector, getSVGIconPath } from '../../utils'
 import * as logic from './logic'
+import {
+  Wrapper,
+  GridWrapper,
+  Card,
+  CommunityIcon,
+  CardTitle,
+  CardDesc,
+  ActivitySpark,
+  Divider,
+  CardFooter,
+} from './styles'
 
 const debug = makeDebugger('C:CommunitiesContent')
+
+const fakeData = [
+  {
+    title: 'js',
+    desc: 'the only language that works on every device on earth',
+    raw: 'js',
+  },
+  {
+    title: 'Elixir',
+    desc: 'Elixir |> for fun',
+    raw: 'elixir',
+  },
+  {
+    title: 'React',
+    desc: 'Elixir |> for fun',
+    raw: 'react',
+  },
+  {
+    title: 'java',
+    desc: 'Elixir |> for fun',
+    raw: 'java',
+  },
+  {
+    title: 'angular',
+    desc: 'give me a chance',
+    raw: 'angular',
+  },
+]
+
+const sparkData = [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]
+const CommunityCard = ({ community }) => (
+  <Card>
+    <CommunityIcon path={getSVGIconPath(community.raw)} />
+    <CardTitle>{community.title}</CardTitle>
+    <CardDesc>{community.desc}</CardDesc>
+    <ActivitySpark>
+      <Trend
+        smooth
+        autoDraw
+        autoDrawDuration={1000}
+        autoDrawEasing="ease-in"
+        data={sparkData}
+        gradient={['#D6ECB2', '#4F966E']}
+        radius={15}
+        strokeWidth={3}
+        strokeLinecap="round"
+      />
+    </ActivitySpark>
+    <Divider />
+    <CardFooter>
+      <div>10k+ 关注</div>
+
+      <div>
+        <Button size="small" type="primary">
+          <Icon type="plus" />关注
+        </Button>
+      </div>
+    </CardFooter>
+  </Card>
+)
+
+const CommunitiesGrid = () => (
+  <GridWrapper>
+    {fakeData.map(community => (
+      <CommunityCard key={community.raw} community={community} />
+    ))}
+  </GridWrapper>
+)
 
 class CommunitiesContentContainer extends React.Component {
   componentWillMount() {
@@ -21,7 +102,11 @@ class CommunitiesContentContainer extends React.Component {
   }
 
   render() {
-    return <div>CommunitiesContent container!</div>
+    return (
+      <Wrapper>
+        <CommunitiesGrid />
+      </Wrapper>
+    )
   }
 }
 
