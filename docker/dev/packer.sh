2b94c5c42d405e4934db7675aaff1fe4441a5b75
mydearxym <mydearxym@qq.com>
2018-07-23 20:33:52 +0800

    refactor packer

--- docker/dev/packer.sh
+++ docker/dev/packer.sh
@@ -2,26 +2,37 @@
 
 ENV="dev"
 
-ARCHIVE_NAME="../docker/${ENV}/web.tar.gz"
+ARCHIVE_NAME="./docker/${ENV}/web.tar.gz"
 PACKER_TMP_DIR="./packer_tmp"
-TOTAL_STEPS=1
+TOTAL_STEPS=4
 
 echo "[Step 1/${TOTAL_STEPS}] creating ${PACKER_TMP_DIR} ..."
 if [ -d "${PACKER_TMP_DIR}" ]; then
+  echo "remove ${PACKER_TMP_DIR}"
   rm -rf "${PACKER_TMP_DIR}"
 fi
 mkdir "${PACKER_TMP_DIR}"
 
 echo "[Step 2/${TOTAL_STEPS}] cp files to ${PACKER_TMP_DIR} ..."
-cp -rf pages lang containers components stores config static utils next.config.js "${PACKER_TMP_DIR}"
+pwd
+# cp -rf pages lang containers components stores config static utils next.config.js .env .babelrc "${PACKER_TMP_DIR}"
+npm run build
+# mv package.json.tmp package.json
+
+cp -rf .next "${PACKER_TMP_DIR}"
 cp package-docker.json "${PACKER_TMP_DIR}/package.json"
 
+cd "${PACKER_TMP_DIR}/.next"
+find . -name "*.js.map" -type f | xargs rm -f
+find . -name "*.hot-update.js" -type f | xargs rm -f
+find . -name "*.hot-update.json" -type f | xargs rm -f
+
 echo "[Step 3/${TOTAL_STEPS}] creating ${ARCHIVE_NAME} ..."
-cd "${PACKER_TMP_DIR}"
-tar czf "${ARCHIVE_NAME}" pages lang containers components stores config static utils package.json next.config.js
+cd ../..
+tar czvf "${ARCHIVE_NAME}" "${PACKER_TMP_DIR}/.next" "${PACKER_TMP_DIR}/package.json"
 
 echo "[Step 4/${TOTAL_STEPS}] cleanup ..."
-cd ../
+pwd
 rm -rf "${PACKER_TMP_DIR}"
 echo "--------------------------"
 echo "done !"
