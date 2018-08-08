b98a84cb128353be0376210e1a3b015dbe573d4a
mydearxym <mydearxym@qq.com>
2018-08-08 12:39:38 +0800

    ci: jest issue
    n

--- .jest.config.js
+++ .jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   setupFiles: ['<rootDir>/.jest.setup.js'],
   coverageDirectory: './coverage/',
+  testURL: 'http://localhost/',
   collectCoverage: true,
   testPathIgnorePatterns: [
     '<rootDir>/.next/',
