182951d8584530aac88606ada8cbd40ec2e1c618
mydearxym <mydearxym@qq.com>
2018-08-20 16:50:55 +0800

    feat: make user profile extendable

--- containers/UserBanner/styles/index.js
+++ containers/UserBanner/styles/index.js
@@ -3,7 +3,9 @@ import { theme } from '../../../utils'
 
 export const BannerContainer = styled.nav`
   position: relative;
-  min-height: 180px;
+  min-height: 200px;
+  padding-top: 8px;
+  padding-bottom: 8px;
   border-bottom: 1px solid tomato;
   display: flex;
   flex-direction: column;
@@ -14,6 +16,8 @@ export const BannerContainer = styled.nav`
   @media (max-height: 800px) {
     min-height: 130px;
   }
+
+  transition: max-height 0.2s;
 `
 
 /* export const TabberWrapper = BaseTabber.extend`` */
@@ -28,5 +32,6 @@ export const UserBriefWrapper = styled.div`
 `
 export const UserContributesWrapper = styled.div`
   width: 40%;
-  max-width: 400px;
+  max-width: 450px;
+  margin-right: 20px;
 `
--- containers/UserBanner/styles/user_brief.js
+++ containers/UserBanner/styles/user_brief.js
@@ -9,6 +9,8 @@ export const Wrapper = styled.div`
 
 export const AvatarWrapper = styled.div`
   margin-right: 12px;
+  display: flex;
+  flex-direction: column;
 `
 
 export const Avatar = styled(Img)`
@@ -16,6 +18,7 @@ export const Avatar = styled(Img)`
   width: 120px;
   height: 120px;
   margin-top: 5px;
+  margin-bottom: 10px;
 `
 
 export const BriefTextWrapper = styled.div`
@@ -33,11 +36,35 @@ export const UserTitle = styled.div`
 export const UserDesc = styled.div`
   color: ${theme('banner.desc')};
   display: flex;
+  font-size: 0.8rem;
+
+  &:hover {
+    cursor: ${({ clickable }) => (clickable ? 'pointer' : '')};
+    font-weight: ${({ clickable }) => (clickable ? 'bolder' : '')};
+  }
+`
+
+export const UserDetailDesc = UserDesc.extend`
+  font-size: 0.9rem;
+  margin-bottom: 6px;
+  margin-top: 8px;
 `
 
 export const DescLable = styled.div`
-  font-size: 0.8rem;
   min-width: 70px;
+  opacity: 0.9;
+`
+
+export const DescIconLable = styled(Img)`
+  fill: ${theme('banner.desc')};
+  width: 16px;
+  height: 16px;
+  margin-right: 10px;
+  margin-top: 2px;
+`
+
+export const DetailToggleLabel = DescIconLable.extend`
+  transform: ${({ reverse }) => (reverse ? 'rotate(180deg)' : '')};
 `
 
 export const SocialSpliter = styled.div`
