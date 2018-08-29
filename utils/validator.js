bb509dc00cdc9f5d37b1aa31a313581f59bbdc09
mydearxym <mydearxym@qq.com>
2018-08-29 21:04:19 +0800

    style: fmt

--- utils/validator.js
+++ utils/validator.js
@@ -1,10 +1,19 @@
 import R from 'ramda'
 
-export const notEmpty = R.compose(R.not, R.isEmpty)
-export const isEmptyValue = R.compose(R.isEmpty, R.trim)
+export const notEmpty = R.compose(
+  R.not,
+  R.isEmpty
+)
+export const isEmptyValue = R.compose(
+  R.isEmpty,
+  R.trim
+)
 export const nilOrEmpty = R.either(R.isNil, isEmptyValue)
 
-export const hasValue = R.compose(R.not, nilOrEmpty)
+export const hasValue = R.compose(
+  R.not,
+  nilOrEmpty
+)
 
 export const isObject = value => {
   const type = typeof value
@@ -13,8 +22,14 @@ export const isObject = value => {
 
 const objHasValue = v => notEmpty(v)
 
-const validObjects = R.compose(R.pickBy(objHasValue), R.pickBy(isObject))
-const validValues = R.compose(R.pickBy(hasValue), R.reject(isObject))
+const validObjects = R.compose(
+  R.pickBy(objHasValue),
+  R.pickBy(isObject)
+)
+const validValues = R.compose(
+  R.pickBy(hasValue),
+  R.reject(isObject)
+)
 
 export const cast = (fields, source) => {
   const casted = R.pick(fields, source)
