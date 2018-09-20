a39cd02c0865e1bd1a0a0edde70f03cfb7cc09ed
mydearxym <mydearxym@qq.com>
2018-09-20 23:17:37 +0800

    chore: update pkg (next.js -> 7.0.0)

--- next.config.js
+++ next.config.js
@@ -5,13 +5,12 @@ const path = require('path')
 const fs = require('fs')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const Dotenv = require('dotenv-webpack')
-const withProgressBar = require('next-progressbar')
 /* eslint-enable */
 const { ANALYZE } = process.env
 // export example
 // https://github.com/zeit/next.js/blob/canary/examples/with-static-export/next.config.js
 
-module.exports = withProgressBar({
+module.exports = {
   exportPathMap: () => {
     return {
       /* '/': { page: '/' }, */
@@ -53,4 +52,4 @@ module.exports = withProgressBar({
 
     return config
   },
-})
+}
