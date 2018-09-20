0c60094ae5c60ef3364015a9409e6b0b7002c216
mydearxym <mydearxym@qq.com>
2018-09-20 23:46:40 +0800

    chore: clean up

--- next.config.js
+++ next.config.js
@@ -1,32 +1,16 @@
 /* eslint-disable */
 require('dotenv').config()
 const path = require('path')
-
 const fs = require('fs')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const Dotenv = require('dotenv-webpack')
 /* eslint-enable */
+
 const { ANALYZE } = process.env
-// export example
-// https://github.com/zeit/next.js/blob/canary/examples/with-static-export/next.config.js
 
 module.exports = {
-  exportPathMap: () => {
-    return {
-      /* '/': { page: '/' }, */
-      // '/ruby': { page: '/', query: { name: 'ruby' } },
-      /* '/cheatsheet': { page: '/', query: { main: 'cheatsheet', sub: '' } }, */
-      /* '/communities': { page: '/', query: { main: 'communities', sub: '' } }, */
-      /* '/js/posts': { page: '/', query: { main: 'js', sub: 'posts' } }, */
-      /* '/intro/i': { page: '/intro', query: { name: 'index' } }, */
-      /* '/intro/feature': { page: '/intro', query: { name: 'feature' } }, */
-      /* '/intro/theme': { page: '/intro', query: { name: 'theme' } }, */
-      /* '/intro/i18n': { page: '/intro', query: { name: 'i18n' } }, */
-      /* '/intro/example': { page: '/intro', query: { name: 'example' } }, */
-      /* '/intro/cmdpanel': { page: '/intro', query: { name: 'cmdpanel' } }, */
-      /* '/intro/graphql': { page: '/intro', query: { name: 'graphql' } }, */
-    }
-  },
+  // https://github.com/zeit/next.js/blob/canary/examples/with-static-export/next.config.js
+  // exportPathMap: () => {},
 
   webpack: (config, { isServer }) => {
     config.plugins = config.plugins || []
