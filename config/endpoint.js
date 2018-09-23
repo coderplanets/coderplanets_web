fabb52180ac3fa408a5fc3b20d63bc9e65c9413e
mydearxym <mydearxym@qq.com>
2018-09-23 15:33:43 +0800

    chore: clean up

--- config/endpoint.js
+++ config/endpoint.js
@@ -7,8 +7,8 @@ const getGraphQLEndpoint = () => {
   /* console.log('process.env.NODE_ENV: ', process.env.NODE_ENV) */
   switch (process.env.GOAL) {
     case 'production':
-      /* return 'http://api.coderplanets.com/graphiql' */
-      return 'http://localhost:4001/graphiql'
+      return 'http://api.coderplanets.com/graphiql'
+    /* return 'http://localhost:4001/graphiql' */
 
     case 'dev':
       return 'http://devapi.coderplanets.com/graphiql'
