ef34763345fdff2374bc45ca8b0b7c19c5ffbd39
mydearxym <mydearxym@qq.com>
2018-10-19 23:08:49 +0800

    feat(JobPage): add CompanyCard

--- containers/JobContent/SideCards.js
+++ containers/JobContent/SideCards.js
@@ -6,10 +6,13 @@ import {
   BadContentRepot,
 } from '../../components'
 
+import CompanyCard from './CommunityCard'
+
 import { Wrapper, ReportWrapper } from './styles/side_cards'
 
 const SideCards = ({ data }) => (
   <Wrapper>
+    <CompanyCard data={data} />
     <AuthorCard user={data.author} />
     <ContentSourceCard data={data} />
     <ReportWrapper>
