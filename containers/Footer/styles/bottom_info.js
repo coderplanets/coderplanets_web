5e74260da1bd62b2be279dee316fb2cb5f0482c4
mydearxym <mydearxym@qq.com>
2018-11-05 09:33:28 +0800

    refactor(Footer): add Thx list to bottom

---
+++ containers/Footer/styles/bottom_info.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+// import Img from '../../../components/Img'
+import { theme, cs } from '../../../utils'
+
+export const Wrapper = styled.div`
+  ${cs.flex('align-center')};
+  justify-content: space-between;
+  padding-left: 5%;
+  padding-right: 5%;
+  background: #d9e6e5;
+  height: 40px;
+  width: 100%;
+`
+export const Copyrights = styled.div`
+  color: ${theme('thread.articleDigest')};
+`
+export const Thanks = styled.div`
+  color: ${theme('thread.articleDigest')};
+`
+export const ThxTitle = styled.div``
