edf6044a2d793fc4eb659df3eb705de8071f0ecf
mydearxym <mydearxym@qq.com>
2018-11-09 10:19:32 +0800

    build(reduce size): by use ContextReplacementPlugin

--- next.config.js
+++ next.config.js
@@ -1,21 +1,28 @@
 /* eslint-disable */
+const webpack = require('webpack')
+
 require('dotenv').config()
 const path = require('path')
 const fs = require('fs')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const Dotenv = require('dotenv-webpack')
-const withCSS = require('@zeit/next-css')
 
 /* eslint-enable */
 
 const { ANALYZE } = process.env
 
-module.exports = withCSS({
+module.exports = {
   // https://github.com/zeit/next.js/blob/canary/examples/with-static-export/next.config.js
   // exportPathMap: () => {},
 
   webpack: (config, { isServer }) => {
     config.plugins = config.plugins || []
+
+    config.plugins.push(new webpack.IgnorePlugin(/(?:\/tests|__mocks)/))
+    // moment locale size is too big
+    config.plugins.push(
+      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /(en)/)
+    )
 
     if (ANALYZE) {
       config.plugins.push(
@@ -38,4 +45,4 @@ module.exports = withCSS({
 
     return config
   },
-})
+}
