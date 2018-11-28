485ab1990bfa0f6a5b5d95131bd11cf203cbbd76
mydearxym <mydearxym@qq.com>
2018-11-29 00:31:19 +0800

    build: production

--- stores/AccountStore/index.js
+++ stores/AccountStore/index.js
@@ -75,8 +75,8 @@ const AccountStore = t
     setSession(user, token) {
       debug('setSession user: ', user)
       debug('setSession token: ', token)
-      if (!token || !user) return false
-      self.isValidSession = true
+      // if (!token || !user) return false
+      // self.isValidSession = true
       BStore.set('user', user)
       BStore.set('token', token)
       BStore.cookie.set('jwtToken', token)
@@ -93,9 +93,7 @@ const AccountStore = t
       self.userSubscribedCommunities = data
     },
     addSubscribedCommunity(community) {
-      const {
-        userSubscribedCommunities: { entries },
-      } = self
+      const { userSubscribedCommunities: { entries } } = self
 
       self.userSubscribedCommunities.entries = R.insert(0, community, entries)
       self.userSubscribedCommunities.totalCount += 1
@@ -118,9 +116,7 @@ const AccountStore = t
     },
 
     removeSubscribedCommunity(community) {
-      const {
-        userSubscribedCommunities: { entries },
-      } = self
+      const { userSubscribedCommunities: { entries } } = self
 
       const index = R.findIndex(R.propEq('id', community.id), entries)
       self.userSubscribedCommunities.entries = R.remove(index, 1, entries)
