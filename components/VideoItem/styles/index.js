f43e180e57645c8986f0c2bcfb262c83111a0ba3
mydearxym <mydearxym@qq.com>
2018-12-30 11:39:41 +0800

    refactor(content-label): videoItem readlabel replace with prefix-label

--- components/VideoItem/styles/index.js
+++ components/VideoItem/styles/index.js
@@ -18,15 +18,6 @@ export const Wrapper = styled.article`
     background: ${theme('thread.articleHover')};
   }
 `
-export const ReadedLabel = styled.div`
-  color: ${theme('thread.articleDigest')};
-  position: absolute;
-  top: 14px;
-  left: -22px;
-  font-size: 1rem;
-  opacity: 0.9;
-  font-style: italic;
-`
 export const PosterWrapper = styled.div`
   position: relative;
   height: 138px;
