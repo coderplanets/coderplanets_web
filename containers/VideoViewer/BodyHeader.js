5ec32706e5de8df17c5e744e7bbafb0a3554484f
mydearxym <mydearxym@qq.com>
2019-01-08 15:44:39 +0800

    fix: RefinedLabel display

--- containers/VideoViewer/BodyHeader.js
+++ containers/VideoViewer/BodyHeader.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import R from 'ramda'
 
 import { ICON_CMD } from '../../config'
 
@@ -18,36 +19,44 @@ import {
 
 import * as logic from './logic'
 
-const BodyHeader = ({ thread, data }) => (
-  <Wrapper>
-    <MoreWrapper>
-      <Popover
-        content={
-          <ArticleActionsPanel
-            thread={thread}
-            entry={data}
-            onPin={logic.onPin}
-            onUndoPin={logic.onUndoPin}
-            onEdit={logic.onEdit}
-            onDelete={logic.onDelete}
-          />
-        }
-        placement="bottomLeft"
-        trigger="click"
-      >
-        <div>
-          <MoreIcon src={`${ICON_CMD}/article_more.svg`} />
-        </div>
-      </Popover>
-    </MoreWrapper>
-    <LablerWrapper>
-      <Labeler />
-    </LablerWrapper>
-    <RefinedLabel>
-      <RefinedIcon src={`${ICON_CMD}/diamond_frame.svg`} />
-      <RefinedText>精 华</RefinedText>
-    </RefinedLabel>
-  </Wrapper>
-)
+const BodyHeader = ({ thread, data }) => {
+  const isRefined = R.contains('refined', R.pluck('title', data.tags))
+
+  return (
+    <Wrapper>
+      <MoreWrapper>
+        <Popover
+          content={
+            <ArticleActionsPanel
+              thread={thread}
+              entry={data}
+              onPin={logic.onPin}
+              onUndoPin={logic.onUndoPin}
+              onEdit={logic.onEdit}
+              onDelete={logic.onDelete}
+            />
+          }
+          placement="bottomLeft"
+          trigger="click"
+        >
+          <div>
+            <MoreIcon src={`${ICON_CMD}/article_more.svg`} />
+          </div>
+        </Popover>
+      </MoreWrapper>
+      <LablerWrapper>
+        <Labeler />
+      </LablerWrapper>
+      {isRefined ? (
+        <RefinedLabel>
+          <RefinedIcon src={`${ICON_CMD}/diamond_frame.svg`} />
+          <RefinedText>精 华</RefinedText>
+        </RefinedLabel>
+      ) : (
+        <div />
+      )}
+    </Wrapper>
+  )
+}
 
 export default BodyHeader
