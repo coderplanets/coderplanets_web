4e10e7ed1d3d548bc2ebe86d6532a9a13a13f419
mydearxym <mydearxym@qq.com>
2019-01-11 13:04:02 +0800

    feat(content page): add Affix effect

--- containers/JobContent/styles/side_cards.js
+++ containers/JobContent/styles/side_cards.js
@@ -4,7 +4,7 @@ import { cs } from '../../../utils'
 
 export const Wrapper = styled.div`
   ${cs.flexColumn()};
-  width: 22%;
+  width: 90%;
 `
 export const ReportWrapper = styled.div`
   padding: 0 10px;
