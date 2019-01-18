58ce586b7fdab382262eb918e5666199d4454f45
mydearxym <mydearxym@qq.com>
2019-01-18 17:30:08 +0800

    chore: clean up

--- containers/ArticleBodyHeader/Linker.js
+++ containers/ArticleBodyHeader/Linker.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 // import { ICON_CMD } from '../../config'
 import { LinkFrom, LinkSource } from './styles'
+import { cutFrom } from '../../utils'
 
 const Linker = ({ addr }) => (
   <React.Fragment>
     {addr ? (
       <LinkFrom href={addr} target="_blank" rel="noopener noreferrer">
         <div>原文地址:&nbsp;</div>
-        <LinkSource>{addr}</LinkSource>
+        <LinkSource>{cutFrom(addr, 30)}</LinkSource>
       </LinkFrom>
     ) : null}
   </React.Fragment>
