74eb2b3a435096fb44cd17e4c52bb45f55b86707
mydearxym <mydearxym@qq.com>
2019-01-25 19:15:52 +0800

    refactor: extract user editable community list

--- containers/UserContent/styles/community_editor_info.js
+++ containers/UserContent/styles/community_editor_info.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 
-import Img from 'components/Img'
 import { theme, cs } from 'utils'
 
 export const Wrapper = styled.div`
@@ -9,18 +8,7 @@ export const Wrapper = styled.div`
   color: ${theme('banner.desc')};
   flex-wrap: wrap;
 `
-export const CommunityPopinfo = styled.div`
-  padding: 5px 10px;
-  color: ${theme('thread.articleTitle')};
-`
-export const CommunityIcon = styled(Img)`
-  width: 20px;
-  height: 20px;
-  display: block;
-  margin-right: 4px;
-  margin-bottom: 10px;
-  ${cs.smokey};
-`
+
 export const Text = styled.div`
   font-size: 0.9rem;
   margin-top: 1px;
