4ae5f1b75acc3cb2f41da6b518679e9873124cc4
mydearxym <mydearxym@qq.com>
2019-01-31 13:10:27 +0800

    refactor(comments): mention in create/reply, closes #348

--- containers/MailsViewer/MentionList.js
+++ containers/MailsViewer/MentionList.js
@@ -24,22 +24,59 @@ import {
   SourcePreview,
   PreviewBody,
   AtLabel,
+  FloorNum,
 } from './styles/mention_list'
 
 import { loadMentions } from './logic'
 
 const getLinkAddr = item => {
-  const { sourceType } = item
-  const thread = sourceType === 'posts' ? 'post' : sourceType
+  const { community, parentType, parentId, sourceType, sourceId } = item
 
-  return `/${item.community}/${thread}/${item.sourceId}`
+  if (!R.isEmpty(parentType)) {
+    // comment
+    return `/${community}/${parentType}/${parentId}`
+  }
+
+  // article
+  const thread = sourceType === 'posts' ? 'post' : sourceType
+  return `/${community}/${thread}/${sourceId}`
+}
+
+const AtMessage = ({ item }) => {
+  const { parentType, floor, sourceType } = item
+  if (!R.isEmpty(parentType)) {
+    if (sourceType === 'comment_reply') {
+      return (
+        <span>
+          <FloorNum>{`#${floor}`}</FloorNum>
+          回复了你
+        </span>
+      )
+    }
+
+    return (
+      <span>
+        在<FloorNum>{`#${floor}`}</FloorNum>
+        @了你
+      </span>
+    )
+  }
+  return <span>中@了你</span>
 }
 
 const MentionList = ({
   data: { entries, pageNumber, pageSize, totalCount },
   readState,
 }) => {
-  if (R.isEmpty(entries)) return <EmptyLabel text="还没有人提到(@)你" />
+  if (R.isEmpty(entries))
+    return (
+      <Wrapper>
+        <InfoWrapper>
+          <ToggleInfo readState={readState} totalCount={totalCount} />
+        </InfoWrapper>
+        <EmptyLabel text="还没有人提到(@)你" />
+      </Wrapper>
+    )
 
   return (
     <Wrapper>
@@ -69,7 +106,9 @@ const MentionList = ({
               <MessageBody>
                 <SourcePreview>
                   <PreviewBody>{item.sourcePreview}</PreviewBody>
-                  <AtLabel>中@了你</AtLabel>
+                  <AtLabel>
+                    <AtMessage item={item} />
+                  </AtLabel>
                 </SourcePreview>
               </MessageBody>
             </Message>
