648594558f9f2f15a3f14ae6e00c87e77123b430
mydearxym <mydearxym@qq.com>
2019-02-07 16:50:22 +0800

    refactor(footer): add mail madal to sponser

--- containers/Footer/BottomInfo.js
+++ containers/Footer/BottomInfo.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 // import { ICON_CMD } from 'config'
 import { Wrapper, Copyrights, Thanks, ThxTitle } from './styles/bottom_info'
+import { toggleBusBanner } from './logic'
 
 const BottomInfo = () => (
   <Wrapper>
     <Copyrights>©2018 All Rights Reserved</Copyrights>
-    <Thanks>
+    <Thanks onClick={toggleBusBanner}>
       <ThxTitle>特别鸣谢： (求赞助)</ThxTitle>
     </Thanks>
   </Wrapper>
