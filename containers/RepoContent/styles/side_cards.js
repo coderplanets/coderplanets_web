e992ee9c43e5eb5e705a12d9534b8359f73f8f71
mydearxym <mydearxym@qq.com>
2019-02-10 18:12:11 +0800

    style(side-cards): add min-width

--- containers/RepoContent/styles/side_cards.js
+++ containers/RepoContent/styles/side_cards.js
@@ -5,6 +5,7 @@ import { cs } from 'utils'
 export const Wrapper = styled.div`
   ${cs.flexColumn()};
   width: 90%;
+  min-width: 250px;
   ${cs.media.tablet`display: none`};
 `
 export const ReportWrapper = styled.div`
