9955d5f4d9ecfb7510458e172618fb76bfc86769
mydearxym <mydearxym@qq.com>
2019-02-10 09:31:00 +0800

    chore: clean up

--- containers/Comments/EditorFooter.js
+++ containers/Comments/EditorFooter.js
@@ -26,57 +26,53 @@ const EditorFooter = ({
   onPreview,
   showFold,
   onFold,
-}) => {
-  console.log('showFold: ', showFold)
+}) => (
+  <InputFooter>
+    {showPreview ? (
+      <InputHelper />
+    ) : (
+      <InputHelper>
+        <div onClick={logic.insertCode}>
+          <HelperIcon src={`${ICON_CMD}/extra_code.svg`} />
+        </div>
+        <div onClick={logic.insertQuote}>
+          <HelperIcon src={`${ICON_CMD}/extra_quote.svg`} />
+        </div>
+        <DocUploader onUploadDone={logic.onUploadImageDone}>
+          <HelperIcon src={`${ICON_CMD}/extra_image.svg`} />
+        </DocUploader>
+      </InputHelper>
+    )}
 
-  return (
-    <InputFooter>
-      {showPreview ? (
-        <InputHelper />
-      ) : (
-        <InputHelper>
-          <div onClick={logic.insertCode}>
-            <HelperIcon src={`${ICON_CMD}/extra_code.svg`} />
-          </div>
-          <div onClick={logic.insertQuote}>
-            <HelperIcon src={`${ICON_CMD}/extra_quote.svg`} />
-          </div>
-          <DocUploader onUploadDone={logic.onUploadImageDone}>
-            <HelperIcon src={`${ICON_CMD}/extra_image.svg`} />
-          </DocUploader>
-        </InputHelper>
+    <InputSubmit>
+      {showFold && (
+        <FoldBtn onClick={onFold}>
+          <FoldArrow src={`${ICON_CMD}/arrow-up-o.svg`} />
+          <FoldText>收起</FoldText>
+        </FoldBtn>
       )}
-
-      <InputSubmit>
-        {showFold && (
-          <FoldBtn onClick={onFold}>
-            <FoldArrow src={`${ICON_CMD}/arrow-up-o.svg`} />
-            <FoldText>收起</FoldText>
-          </FoldBtn>
-        )}
-        <Space right="14px" />
-        {showPreview ? (
-          <Button type="primary" ghost size="small" onClick={onBackEdit}>
-            返回编辑
-          </Button>
-        ) : (
-          <Button type="primary" ghost size="small" onClick={onPreview}>
-            预<Space right="5px" />览
-          </Button>
-        )}
-        <Space right="10px" />
-        {!loading ? (
-          <Button type="primary" size="small" onClick={onCreate}>
-            提<Space right="5px" />交
-          </Button>
-        ) : (
-          <Button type="primary" size="small">
-            <Icon type="loading" />提<Space right="5px" />交
-          </Button>
-        )}
-      </InputSubmit>
-    </InputFooter>
-  )
-}
+      <Space right="14px" />
+      {showPreview ? (
+        <Button type="primary" ghost size="small" onClick={onBackEdit}>
+          返回编辑
+        </Button>
+      ) : (
+        <Button type="primary" ghost size="small" onClick={onPreview}>
+          预<Space right="5px" />览
+        </Button>
+      )}
+      <Space right="10px" />
+      {!loading ? (
+        <Button type="primary" size="small" onClick={onCreate}>
+          提<Space right="5px" />交
+        </Button>
+      ) : (
+        <Button type="primary" size="small">
+          <Icon type="loading" />提<Space right="5px" />交
+        </Button>
+      )}
+    </InputSubmit>
+  </InputFooter>
+)
 
 export default EditorFooter
