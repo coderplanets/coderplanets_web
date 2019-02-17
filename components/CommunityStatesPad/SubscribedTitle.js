10269fec26ad9bbbd8acd6750bd3715abe83101e
mydearxym <mydearxym@qq.com>
2019-02-17 10:55:46 +0800

    fix(community-banner): ban user unsub home community

--- components/CommunityStatesPad/SubscribedTitle.js
+++ components/CommunityStatesPad/SubscribedTitle.js
@@ -6,23 +6,37 @@ import Popover from 'components/Popover'
 import { NumberTitle } from './styles'
 import { PopoverInfo, PopTitle, PopDesc } from './styles/subscribed_title'
 
+const SubscribedBtn = ({ community, onUndoSubscribe }) => {
+  if (community.raw === 'home') {
+    return (
+      <NumberTitle small readOnly>
+        已关注
+      </NumberTitle>
+    )
+  }
+
+  return (
+    <Popover
+      placement="bottom"
+      trigger="hover"
+      content={
+        <PopoverInfo>
+          <PopTitle>点击可取消关注</PopTitle>
+          <PopDesc>取消关注后该社区将不会出现在你的左侧列表中</PopDesc>
+        </PopoverInfo>
+      }
+    >
+      <NumberTitle onClick={onUndoSubscribe.bind(this, community)} small>
+        已关注
+      </NumberTitle>
+    </Popover>
+  )
+}
+
 const SubscribedTitle = ({ community, onSubscribe, onUndoSubscribe }) => (
   <React.Fragment>
     {community.viewerHasSubscribed ? (
-      <Popover
-        placement="bottom"
-        trigger="hover"
-        content={
-          <PopoverInfo>
-            <PopTitle>点击可取消关注</PopTitle>
-            <PopDesc>取消关注后该社区将不会出现在你的左侧列表中</PopDesc>
-          </PopoverInfo>
-        }
-      >
-        <NumberTitle onClick={onUndoSubscribe.bind(this, community)} small>
-          已关注
-        </NumberTitle>
-      </Popover>
+      <SubscribedBtn community={community} onUndoSubscribe={onUndoSubscribe} />
     ) : (
       <Popover
         placement="bottom"
