67ba348f339141a1638ceb683f001954e17e7e84
mydearxym <mydearxym@qq.com>
2019-03-16 14:17:20 +0800

    chore(pkgs): update & resolve new syntax

--- containers/PostsThread/index.js
+++ containers/PostsThread/index.js
@@ -6,7 +6,7 @@
 
 import React from 'react'
 import { inject, observer } from 'mobx-react'
-import Waypoint from 'react-waypoint'
+import { Waypoint } from 'react-waypoint'
 import R from 'ramda'
 import { Affix } from 'antd'
 
