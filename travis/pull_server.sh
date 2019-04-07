b6ecb8e7a910ab8d3248465595780c4eda8844e0
mydearxym <mydearxym@qq.com>
2019-04-07 15:04:30 +0800

    build(tmp): cypress mock idea

---
+++ travis/pull_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export WORKING_DIR=`pwd`
+echo "> Working dir: $WORKING_DIR"
+
+echo "> Getting data..."
+git clone https://github.com/coderplanets/coderplanets_server.git
+
+echo "> done"
