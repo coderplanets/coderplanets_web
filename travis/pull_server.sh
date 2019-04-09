b74d34add920a2b9199919592fabae4d2e2aaea9
mydearxym <mydearxym@qq.com>
2019-04-09 21:20:14 +0800

    ci(travis): debug 103

--- travis/pull_server.sh
+++ travis/pull_server.sh
@@ -4,6 +4,8 @@ export WORKING_DIR=`pwd`
 echo "> Working dir: $WORKING_DIR"
 cd ..
 touch server_touched_files
+echo "> install nvm"
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 echo "> check nvm"
 nvm --version
 git clone https://github.com/coderplanets/coderplanets_server.git
