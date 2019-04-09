101d01870fe4c233b2c67f5af54114d3032f61a6
mydearxym <mydearxym@qq.com>
2019-04-09 21:16:03 +0800

    ci(travis): debug 102

--- travis/pull_server.sh
+++ travis/pull_server.sh
@@ -4,9 +4,8 @@ export WORKING_DIR=`pwd`
 echo "> Working dir: $WORKING_DIR"
 cd ..
 touch server_touched_files
-echo "> check node"
-node --version
-echo "> check node"
+echo "> check nvm"
+nvm --version
 git clone https://github.com/coderplanets/coderplanets_server.git
 pwd
 ls
