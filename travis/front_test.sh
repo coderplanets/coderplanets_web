8cd70c40e84475e7fc9f37cbbcaf154dda1ebb73
mydearxym <mydearxym@qq.com>
2019-04-09 20:40:07 +0800

    ci(travis): debug

---
+++ travis/front_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export WORKING_DIR=`pwd`
+echo "> Working dir: $WORKING_DIR"
+cd ..
+touch front_touched_files
+ls
+echo "> done"
--- travis/pull_server.sh
+++ travis/pull_server.sh
@@ -3,6 +3,8 @@
 export WORKING_DIR=`pwd`
 echo "> Working dir: $WORKING_DIR"
 cd ..
+touch server_touched_files
+ls
 echo "> Getting server repo..."
 git clone https://github.com/coderplanets/coderplanets_server.git
 
