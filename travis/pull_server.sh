c031a749b9572ee7361a40344e7334f02e9f17b7
mydearxym <mydearxym@qq.com>
2019-04-10 22:59:19 +0800

    ci(traivs): debug 205

--- travis/pull_server.sh
+++ travis/pull_server.sh
@@ -9,9 +9,9 @@ git clone https://github.com/coderplanets/coderplanets_server.git
 cd coderplanets_server
 mix deps.get
 echo "> setup test server"
-MIX_ENV=test mix ecto.setup
+MIX_ENV=ci mix ecto.setup
 echo "> running test server"
-MIX_ENV=test mix phx.server
+MIX_ENV=ci mix phx.server
 cd ..
 pwd
 ls -ll
