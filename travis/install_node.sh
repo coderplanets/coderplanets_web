ec7b7d44ff33c0beb2d5aa8ed88dd8f4afbbf310
mydearxym <mydearxym@qq.com>
2019-04-15 21:18:30 +0800

    ci(travis): debug node version

--- travis/install_node.sh
+++ travis/install_node.sh
@@ -2,10 +2,12 @@
 
 echo "> install nvm"
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
-[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh > /dev/null
+[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
 echo "> check nvm"
 nvm --version
 echo "> use node 10.10"
-nvm install 10.10.0 > /dev/null
+nvm install 10.10.0
+nvm use 10.10.0
 echo "> install node done"
+node --version
 pwd
