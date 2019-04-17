c393a3e0e4260181f6a3e777299bbbafdd632df5
mydearxym <mydearxym@qq.com>
2019-04-17 12:55:05 +0800

    test(cypress): add custom cmd to simplify selector

--- cypress/integration/layout.spec.js
+++ cypress/integration/layout.spec.js
@@ -4,18 +4,18 @@ describe('basic layout', () => {
     cy.visit('/home/posts')
   })
 
-  it('basic layout shoud be seen', () => {
-    cy.get('[data-testid=header]').should('be.visible')
-    cy.get('[data-testid=sidebar]').should('be.visible')
+  it('basic layout shoud be visible', () => {
+    cy.id('header').should('be.visible')
+    cy.id('sidebar').should('be.visible')
 
-    cy.get('[data-testid=community-banner]').should('be.visible')
-    cy.get('[data-testid=community-content]').should('be.visible')
+    cy.id('community-banner').should('be.visible')
+    cy.id('community-content').should('be.visible')
 
-    cy.get('[data-testid=footer]').should('be.visible')
+    cy.id('footer').should('be.visible')
   })
 
   it('header content', () => {
-    cy.get('[data-testid=header-search]').should('be.visible')
-    cy.get('[data-testid=header-search-icon]').should('be.visible')
+    cy.id('header-search').should('be.visible')
+    cy.id('header-search-icon').should('be.visible')
   })
 })
