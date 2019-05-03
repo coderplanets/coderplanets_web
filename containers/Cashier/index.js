3780b542c219b7411a5a83424493739c4671c2ba
mydearxym <mydearxym@qq.com>
2019-05-03 22:02:41 +0800

    refactor(hooks): convert cashier

--- containers/Cashier/index.js
+++ containers/Cashier/index.js
@@ -5,61 +5,56 @@
  */
 
 import React from 'react'
-import { inject, observer } from 'mobx-react'
+import { inject } from 'mobx-react'
+import { observer } from 'mobx-react-lite'
+
+import { makeDebugger, storePlug } from 'utils'
 
 import Modal from 'components/Modal'
-// import PaymentView from './PaymentView'
-import { makeDebugger, storePlug } from 'utils'
 import Sidebar from './Sidebar'
 import Content from './Content'
 
 import { Wrapper } from './styles'
 
-import * as logic from './logic'
+import { useInit, onClose } from './logic'
 
 /* eslint-disable-next-line */
 const debug = makeDebugger('C:Cashier')
 
-class CashierContainer extends React.Component {
-  componentDidMount() {
-    const { cashier } = this.props
-    logic.init(cashier)
-  }
+const CashierContainer = ({ cashier }) => {
+  useInit(cashier)
 
-  render() {
-    const { cashier } = this.props
-    const {
-      show,
-      accountInfo,
-      sidebarView,
-      contentView,
-      subContentView,
-      paymentMethod,
-      amount,
-      transferAccount,
-    } = cashier
+  const {
+    show,
+    accountInfo,
+    sidebarView,
+    contentView,
+    subContentView,
+    paymentMethod,
+    amount,
+    transferAccount,
+  } = cashier
 
-    return (
-      <Modal width="520px" show={show} showCloseBtn onClose={logic.onClose}>
-        <Wrapper>
-          <Sidebar
-            accountInfo={accountInfo}
-            sidebarView={sidebarView}
-            subContentView={subContentView}
-            paymentMethod={paymentMethod}
-            amount={amount}
-          />
-          <Content
-            contentView={contentView}
-            subContentView={subContentView}
-            amount={amount}
-            paymentMethod={paymentMethod}
-            transferAccount={transferAccount}
-          />
-        </Wrapper>
-      </Modal>
-    )
-  }
+  return (
+    <Modal width="520px" show={show} showCloseBtn onClose={onClose}>
+      <Wrapper>
+        <Sidebar
+          accountInfo={accountInfo}
+          sidebarView={sidebarView}
+          subContentView={subContentView}
+          paymentMethod={paymentMethod}
+          amount={amount}
+        />
+        <Content
+          contentView={contentView}
+          subContentView={subContentView}
+          amount={amount}
+          paymentMethod={paymentMethod}
+          transferAccount={transferAccount}
+        />
+      </Wrapper>
+    </Modal>
+  )
 }
 
 export default inject(storePlug('cashier'))(observer(CashierContainer))
