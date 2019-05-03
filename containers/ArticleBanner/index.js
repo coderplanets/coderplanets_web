990af2f169e860153bfb5d62c34e5e1a0d3d2913
mydearxym <mydearxym@qq.com>
2019-05-03 21:55:43 +0800

    refactor(hooks): covert Article-related part

--- containers/ArticleBanner/index.js
+++ containers/ArticleBanner/index.js
@@ -6,14 +6,16 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { inject, observer } from 'mobx-react'
+import { inject } from 'mobx-react'
+import { observer } from 'mobx-react-lite'
 import R from 'ramda'
 import TimeAgo from 'timeago-react'
+
+import { makeDebugger, storePlug } from 'utils'
 
 import DotDivider from 'components/DotDivider'
 import Maybe from 'components/Maybe'
 import { Space } from 'components/BaseStyled'
-import { makeDebugger, storePlug } from 'utils'
 import FavoritesCats from '../FavoritesCats'
 
 import Title from './Title'
@@ -22,73 +24,68 @@ import MiddleInfo from './MiddleInfo'
 
 import { Wrapper, BannerContent, Brief, Desc } from './styles'
 
-import * as logic from './logic'
+import { useInit } from './logic'
 
 /* eslint-disable-next-line */
 const debug = makeDebugger('C:ArticleBanner')
 
-class ArticleBannerContainer extends React.Component {
-  componentDidMount() {
-    const { articleBanner } = this.props
-    logic.init(articleBanner)
-  }
+const ArticleBannerContainer = ({
+  articleBanner,
+  showStar,
+  showWordCount,
+  showLastSync,
+}) => {
+  useInit(articleBanner)
 
-  componentWillUnmount() {
-    logic.uninit()
-  }
+  const {
+    activeThread,
+    viewingData,
+    starLoading,
+    favoriteLoading,
+  } = articleBanner
 
-  render() {
-    const { articleBanner, showStar, showWordCount, showLastSync } = this.props
-    const {
-      activeThread,
-      viewingData,
-      starLoading,
-      favoriteLoading,
-    } = articleBanner
-
-    return (
-      <Wrapper>
-        <FavoritesCats />
-        {R.isNil(viewingData.id) ? null : (
-          <BannerContent>
-            <Brief>
-              <Title thread={activeThread} data={viewingData} />
-              <MiddleInfo thread={activeThread} data={viewingData} />
-              <Desc>
-                发布于:
-                <Space left="3px" right="3px" />
-                <TimeAgo datetime={viewingData.insertedAt} locale="zh_CN" />
-                <Maybe test={showWordCount}>
-                  <React.Fragment>
-                    <DotDivider />
-                    字数: {viewingData.length}
-                  </React.Fragment>
-                </Maybe>
-                <Maybe test={showLastSync}>
-                  <React.Fragment>
-                    <DotDivider />
-                    最后同步:
-                    <Space left="3px" right="3px" />
-                    {viewingData.lastSync ? (
-                      <TimeAgo datetime={viewingData.lastSync} locale="zh_CN" />
-                    ) : (
-                      <span>--</span>
-                    )}
-                  </React.Fragment>
-                </Maybe>
-              </Desc>
-            </Brief>
-            <ReactionNumbers
-              data={viewingData}
-              starLoading={starLoading}
-              favoriteLoading={favoriteLoading}
-              showStar={showStar}
-            />
-          </BannerContent>
-        )}
-      </Wrapper>
-    )
-  }
+  return (
+    <Wrapper>
+      <FavoritesCats />
+      {!R.isNil(viewingData.id) && (
+        <BannerContent>
+          <Brief>
+            <Title thread={activeThread} data={viewingData} />
+            <MiddleInfo thread={activeThread} data={viewingData} />
+            <Desc>
+              发布于:
+              <Space left="3px" right="3px" />
+              <TimeAgo datetime={viewingData.insertedAt} locale="zh_CN" />
+              <Maybe test={showWordCount}>
+                <React.Fragment>
+                  <DotDivider />
+                  字数: {viewingData.length}
+                </React.Fragment>
+              </Maybe>
+              <Maybe test={showLastSync}>
+                <React.Fragment>
+                  <DotDivider />
+                  最后同步:
+                  <Space left="3px" right="3px" />
+                  {viewingData.lastSync ? (
+                    <TimeAgo datetime={viewingData.lastSync} locale="zh_CN" />
+                  ) : (
+                    <span>--</span>
+                  )}
+                </React.Fragment>
+              </Maybe>
+            </Desc>
+          </Brief>
+          <ReactionNumbers
+            data={viewingData}
+            starLoading={starLoading}
+            favoriteLoading={favoriteLoading}
+            showStar={showStar}
+          />
+        </BannerContent>
+      )}
+    </Wrapper>
+  )
 }
 
 ArticleBannerContainer.propTypes = {
