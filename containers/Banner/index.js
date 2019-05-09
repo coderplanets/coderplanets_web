6851c95c4edbde16fb6a18cf6edf026f5fa0fdf5
mydearxym <mydearxym@qq.com>
2019-05-10 00:37:06 +0800

    refactor(hooks): more containers using it

--- containers/Banner/index.js
+++ containers/Banner/index.js
@@ -15,7 +15,7 @@ import CommunityBanner from '@containers/CommunityBanner'
 import CheatsheetRootBanner from './CheatsheetRootBanner'
 import ActivitiesRootBanner from './ActivitiesRootBanner'
 
-import init from './logic'
+import { useInit } from './logic'
 
 /* eslint-disable-next-line */
 const debug = makeDebugger('C:Banner')
@@ -59,22 +59,11 @@ const BannerContent = ({ curRoute }) => {
   }
 }
 
-class BannerContainer extends React.Component {
-  constructor(props) {
-    super(props)
+const BannerContainer = ({ banner }) => {
+  useInit(banner)
+  const { curRoute } = banner
 
-    const { banner } = props
-    init(banner)
-  }
-
-  render() {
-    const { banner } = this.props
-    const { curRoute } = banner
-    // const { mainPath } = curRoute
-    // debug('detail ---> ', detail)
-
-    return <BannerContent curRoute={curRoute} banner={banner} />
-  }
+  return <BannerContent curRoute={curRoute} banner={banner} />
 }
 
 export default connectStore(BannerContainer)
