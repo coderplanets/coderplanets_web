6b281e63b57608b77ed89853e3d769784d23c1aa
mydearxym <mydearxym@qq.com>
2019-05-29 21:45:37 +0800

    refactor(pages): make ssr parse jwt based on server/client

--- pages/repo.js
+++ pages/repo.js
@@ -22,6 +22,7 @@ import GAWraper from '@components/GAWraper'
 import ErrorPage from '@components/ErrorPage'
 
 import {
+  getJwtToken,
   nilOrEmpty,
   makeGQClient,
   getMainPath,
@@ -30,7 +31,6 @@ import {
   TYPE,
   ROUTE,
   THREAD,
-  BStore,
   ssrAmbulance,
   parseTheme,
 } from '@utils'
@@ -44,7 +44,7 @@ global.Intl = require('intl')
 async function fetchData(props, opt) {
   const { realname } = R.merge({ realname: true }, opt)
 
-  const token = realname ? BStore.cookie.from_req(props.req, 'jwtToken') : null
+  const token = realname ? getJwtToken(props) : null
   const gqClient = makeGQClient(token)
   const userHasLogin = nilOrEmpty(token) === false
 
