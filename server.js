570483044de2dc48dbaf23c8addc205b990931a2
mydearxym <mydearxym@qq.com>
2019-06-15 12:12:52 +0800

    ci(front): build the project before run cy-test

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const dev = process.env.NODE_ENV !== 'production'
+const dev =
+  process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'ci'
 
 const next = require('next')
 const express = require('express')
