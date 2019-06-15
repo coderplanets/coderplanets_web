f0f9abfab5371052111ab4682b248834622f142d
mydearxym <mydearxym@qq.com>
2019-06-15 16:15:33 +0800

    ci(fix): downgrade next.js to 8.1.0 && use node 12

--- travis/install_node.sh
+++ travis/install_node.sh
@@ -5,9 +5,9 @@ wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | 
 [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
 echo "> check nvm"
 nvm --version
-echo "> use node 10.10"
-nvm install 10.10.0
-nvm alias default 10.10.0
+echo "> use node 12.4.0"
+nvm install 12.4.0
+nvm alias default 12.4.0
 echo "> install node done"
 node --version
 pwd
