def802d0d6b55f5f2ea9093d42acdec0539f4e53
Ji Jie <c141028@protonmail.com>
2019-06-15 16:37:34 +0800

    fix(home-page): fix a typo in website's description (#484)

--- config/next_seo.js
+++ config/next_seo.js
@@ -1,13 +1,13 @@
 export default {
   title: 'coderplanets',
-  description: 'the most sexy community for developers, ever.',
+  description: 'the sexiest community for developers, ever.',
   cannotical: 'https://coderplanets.com',
   openGraph: {
     type: 'website',
     locale: 'en_IE',
     url: 'https://coderplanets.com',
     site_name: 'coderplanets',
-    description: 'the most sexy community for developers, ever.',
+    description: 'the sexiest community for developers, ever.',
   },
   /*
      twitter: {
