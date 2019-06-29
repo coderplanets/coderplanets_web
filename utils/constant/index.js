1d78dbee69e78a92b586a8aaf8994f18cf5d88bf
mydearxym <mydearxym@qq.com>
2019-06-29 18:42:09 +0800

    fix(utils): export constant clean up

--- utils/constant/index.js
+++ utils/constant/index.js
@@ -1,5 +1,18 @@
-export { default as TYPE } from './type'
-export { default as EVENT } from './event'
-export { default as ERR } from './err'
+import TYPE from './type'
+import EVENT from './event'
+import ERR from './err'
+import { THREAD, COMMUNITY_SPEC_THREADS } from './thread'
 
-export { THREAD, COMMUNITY_SPEC_THREADS } from './thread'
+// export { default as TYPE } from './type'
+// export { default as EVENT } from './event'
+// export { default as ERR } from './err'
+
+// export { THREAD, COMMUNITY_SPEC_THREADS } from './thread'
+
+export default {
+  TYPE,
+  EVENT,
+  ERR,
+  THREAD,
+  COMMUNITY_SPEC_THREADS,
+}
