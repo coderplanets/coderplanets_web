fafbede6c18cc82180d4ea5bcdecd3720b0b46b8
mydearxym <mydearxym@qq.com>
2019-07-17 12:26:43 +0800

    fix(sidebar): jumpy in header part

--- containers/Sidebar/Header.js
+++ containers/Sidebar/Header.js
@@ -7,6 +7,7 @@ import PinButton from './PinButton'
 
 import {
   Wrapper,
+  InnerWrapper,
   HeaderFuncs,
   SiteLogoWrapper,
   ExploreWrapper,
@@ -18,22 +19,24 @@ import {
 
 const Header = ({ pin }) => (
   <Wrapper pin={pin}>
-    <HeaderFuncs>
-      <SiteLogoWrapper pin={pin}>
-        <SiteLogo src={`${ICON_BASE}/sidebar/everyday.svg`} />
-      </SiteLogoWrapper>
-      <ExploreWrapper pin={pin}>
-        <a href="/communities" rel="noopener noreferrer" target="_blank">
-          <Button size="small" type="primary" ghost>
-            <ExploreContent>
-              <ExploreIcon src={`${ICON_CMD}/telescope.svg`} />
-              <ExploreText>Explore All</ExploreText>
-            </ExploreContent>
-          </Button>
-        </a>
-      </ExploreWrapper>
-    </HeaderFuncs>
-    <PinButton pin={pin} />
+    <InnerWrapper>
+      <HeaderFuncs>
+        <SiteLogoWrapper pin={pin}>
+          <SiteLogo src={`${ICON_BASE}/sidebar/everyday.svg`} />
+        </SiteLogoWrapper>
+        <ExploreWrapper pin={pin}>
+          <a href="/communities" rel="noopener noreferrer" target="_blank">
+            <Button size="small" type="primary" ghost>
+              <ExploreContent>
+                <ExploreIcon src={`${ICON_CMD}/telescope.svg`} />
+                <ExploreText>Explore All</ExploreText>
+              </ExploreContent>
+            </Button>
+          </a>
+        </ExploreWrapper>
+      </HeaderFuncs>
+      <PinButton pin={pin} />
+    </InnerWrapper>
   </Wrapper>
 )
 
