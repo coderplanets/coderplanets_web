ae971cd64dc9585d9f87fc3675abe93f5082b67a
mydearxym <mydearxym@qq.com>
2019-09-04 22:37:01 +0800

    refactor(editor-playground): basic style setup

--- src/containers/RichEditor/styles/index.js
+++ src/containers/RichEditor/styles/index.js
@@ -3,5 +3,7 @@ import styled from 'styled-components'
 // import Img from '@components/Img'
 // import { theme } from '@utils'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  background: white;
+`
 export const Title = styled.div``
