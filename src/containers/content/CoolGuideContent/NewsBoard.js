4925ca54538806566dc389e6389b3415908a07c1
mydearxym <mydearxym@qq.com>
2020-02-28 23:17:19 +0800

    style(cool-navi): make NewsBoard's shadow scroll effect look great

--- src/containers/content/CoolGuideContent/NewsBoard.js
+++ src/containers/content/CoolGuideContent/NewsBoard.js
@@ -1,4 +1,5 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef, useCallback } from 'react'
+import { Waypoint } from 'react-waypoint'
 
 import { useCustomScroll } from '@hooks'
 
@@ -10,25 +11,50 @@ import {
   Wrapper,
   NewsWrapper,
   NewsInnerWrapper,
+  LeftShadowBar,
+  RightShadowBar,
+  ShadowBarHolder,
   FooterWrapper,
 } from './styles/news_board'
 
 const NewsBoard = () => {
+  const [showLeftShadow, setShowLeftShadow] = useState(false)
+  const [showRightShadow, setShowRightShadow] = useState(true)
+
+  const handleShowLeftShadow = useCallback(() => setShowLeftShadow(true), [])
+  const handleHideLeftShadow = useCallback(() => setShowLeftShadow(false), [])
+
+  const handleShowRightShadow = useCallback(() => setShowRightShadow(true), [])
+  const handleHideRightShadow = useCallback(() => setShowRightShadow(false), [])
+
   const ref = useRef(null)
   useCustomScroll(ref, { scrollbars: { autoHide: 'never' } })
 
   return (
     <Wrapper>
+      {showLeftShadow && <LeftShadowBar />}
       <NewsWrapper ref={ref}>
         <NewsInnerWrapper>
+          <Waypoint
+            horizontal
+            onEnter={handleHideLeftShadow}
+            onLeave={handleShowLeftShadow}
+          />
           <FeedsBar title="Github Trending" numIndex={0} />
           <FeedsBar title="国外社区动态" numIndex={1} />
           <FeedsBar title="国内社区动态" numIndex={2} />
           <FeedsBar title="象牙塔" numIndex={3} />
           <FeedsBar title="独立博客" numIndex={4} />
           <FeedsBar title="播客" numIndex={5} />
+          <ShadowBarHolder />
+          <Waypoint
+            horizontal
+            onEnter={handleHideRightShadow}
+            onLeave={handleShowRightShadow}
+          />
         </NewsInnerWrapper>
       </NewsWrapper>
+      {showRightShadow && <RightShadowBar />}
       <FooterWrapper>
         <Footer />
       </FooterWrapper>
--- src/containers/content/CoolGuideContent/styles/news_board.js
+++ src/containers/content/CoolGuideContent/styles/news_board.js
@@ -5,6 +5,7 @@ import { cs } from '@utils'
 
 export const Wrapper = styled.div`
   width: 100%;
+  position: relative;
 `
 export const NewsWrapper = styled.div`
   width: 100%;
@@ -13,6 +14,35 @@ export const NewsWrapper = styled.div`
 export const NewsInnerWrapper = styled.div`
   ${cs.flex()};
   height: 100%;
+`
+const ShadowBar = styled.div`
+  position: absolute;
+  top: 5px;
+  height: 88vh;
+  width: 50px;
+  background: -webkit-radial-gradient(
+    0% 50%,
+    100% 60%,
+    #022029 0%,
+    transparent 100%
+  );
+  border-left: 1px solid;
+  border-color: #084255;
+  z-index: 1;
+`
+export const LeftShadowBar = styled(ShadowBar)`
+  left: 0px;
+  z-index: 2;
+`
+export const RightShadowBar = styled(ShadowBar)`
+  right: 0;
+  transform: rotate(180deg);
+`
+// Waypoint hack
+export const ShadowBarHolder = styled.div`
+  width: 1px;
+  height: 100%;
+  border: 1px solid transparent;
 `
 export const FooterWrapper = styled.div`
   width: 100%;
