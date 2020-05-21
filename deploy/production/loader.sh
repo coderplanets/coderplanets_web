5b74b56b21f34b37a8b79dfa1c79a5463f244b33
mydearxym <mydearxym@qq.com>
2020-05-21 19:32:09 +0800

    chore(mem-leaks): add --max-memory-restart option to pm2

--- deploy/production/loader.sh
+++ deploy/production/loader.sh
@@ -3,7 +3,7 @@
 cd /root/web/
 
 # make build.prod
-pm2-runtime start npm --name "coderplanets_web" -- run serve.prod &
+pm2-runtime start --max-memory-restart 1G npm --name "coderplanets_web" -- run serve.prod &
 
 while true
 do
