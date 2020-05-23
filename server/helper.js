014568e19c29820c7b5b93c15606865d44df5f03
mydearxym <mydearxym@qq.com>
2020-05-23 22:10:01 +0800

    refactor(ssr-server): only cache in dev mode && enhance _error / _app

--- server/helper.js
+++ server/helper.js
@@ -7,26 +7,37 @@ const uuidv4 = require('uuid/v4')
 const app = require('./app')
 const CONFIG = require('../config/config.json')
 
-const renderAndCache = cacheableResponse({
-  ttl: CONFIG.SSR_CACHE_TIME,
-  get: async ({ req, res, path }) => {
+const renderAndCache = ({ req, res, path }) => {
+  // do not cache in dev mode
+  if (dev) {
     const pagePath = path || req.path
-    const data = await app.renderToHTML(req, res, pagePath, {
+    return app.render(req, res, pagePath, {
       ...req.query,
       ...req.params,
     })
+  }
 
-    // Add here custom logic for when you do not want to cache the page, for
-    // example when the page returns a 404 status code:
-    if (res.statusCode === 404) {
-      res.end(data)
-      return
-    }
+  return cacheableResponse({
+    ttl: CONFIG.SSR_CACHE_TIME,
+    get: async ({ req, res, path }) => {
+      const pagePath = path || req.path
+      const data = await app.renderToHTML(req, res, pagePath, {
+        ...req.query,
+        ...req.params,
+      })
 
-    return { data }
-  },
-  send: ({ data, res }) => res.send(data),
-})
+      // Add here custom logic for when you do not want to cache the page, for
+      // example when the page returns a 404 status code:
+      if (res.statusCode === 404) {
+        res.end(data)
+        return
+      }
+
+      return { data }
+    },
+    send: ({ data, res }) => res.send(data),
+  })({ req, res, path })
+}
 
 // redirect all the www request to non-www addr
 const redirectToNakedUrl = (req, res, next) => {
---
+++ src/pages/_404_not_work.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+// import CrashErrorHint from '@/components/CrashErrorHint'
+
+export default function Custom404() {
+  return <h1>404 - Page Not Found</h1>
+}
