185b73dfb1cd3512f1047e5c9d5be29ece75aaec
xieyimian <mydearxym@qq.com>
2020-06-20 08:04:00 +0800

    fix(sidebar): wrong position fixed when wide screen (#765)

--- src/containers/Sidebar/styles/index.js
+++ src/containers/Sidebar/styles/index.js
@@ -12,13 +12,12 @@ export const Wrapper = styled.aside.attrs((props) => ({
   position: fixed;
   height: 100vh;
   top: 0;
-  left: 0;
+  /* left: 0; */
   width: ${({ pin }) => (pin ? '260px' : '56px')};
   box-shadow: ${({ pin }) => (pin ? '3px 0 20px rgba(0, 0, 0, 0.2); ' : '')};
   background: ${theme('sidebar.bg')};
   border-color: ${theme('sidebar.borderColor')};
   z-index: ${cs.zIndex.sidebar};
-  // overflow: hidden;
 
   transition: width 0.2s, opacity 0.8s, box-shadow 0.1s linear 0.1s,
     background-color 0.3s;
