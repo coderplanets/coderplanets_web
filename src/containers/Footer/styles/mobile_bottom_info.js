3d32a4313a775ccae90e0c23b91280556f1e399d
mydearxym <mydearxym@qq.com>
2020-09-03 22:44:40 +0800

    refactor(Footer): adjust for mobile view

--- src/containers/Footer/styles/mobile_bottom_info.js
+++ src/containers/Footer/styles/mobile_bottom_info.js
@@ -10,7 +10,7 @@ const InfoSection = styled.div`
 `
 export const LinkInfoWrapper = styled(InfoSection)``
 export const SiteInfoWrapper = styled(InfoSection)`
-  background-color: ${theme('footer.bottomBg')};
+  /* background-color: ${theme('footer.bottomBg')}; */
 `
 export const Item = styled.a`
   color: ${theme('footer.text')};
@@ -26,22 +26,3 @@ export const ItemBtn = styled.div`
   border-radius: 5px;
   padding: 0 4px;
 `
-export const BannerWrapper = styled.div`
-  ${cs.flex('align-center', 'justify-between')};
-  padding-left: 6%;
-  padding-right: 6%;
-  background-color: ${theme('footer.bottomBg')};
-  height: 80px;
-  width: 100%;
-  display: none;
-  ${cs.media.tablet`display: flex;`};
-`
-export const Thanks = styled.div`
-  ${cs.flex('align-both')};
-  color: ${theme('thread.articleDigest')};
-  border: 1px dashed;
-  height: 80%;
-  width: 100%;
-  border-radius: 5px;
-`
-export const ThxTitle = styled.div``
