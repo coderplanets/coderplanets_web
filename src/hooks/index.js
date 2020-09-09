cd5ffed2b55168a008e6b796c1f9c1df7107f888
mydearxym <mydearxym@qq.com>
2020-09-09 11:05:13 +0800

    feat(hooks): add langPress hook for mobile action

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -6,6 +6,7 @@ export { default as useScroll } from './useScroll'
 export { default as useResize } from './useResize'
 export { default as useCustomScroll } from './useCustomScroll'
 export { default as useOutsideClick } from './useOutsideClick'
+export { default as useLangPress } from './useLangPress'
 export { default as useTrans } from './useTrans'
 
 export { default as useNetwork } from 'react-use/lib/useNetwork'
---
+++ src/hooks/useLangPress.js
@@ -0,0 +1,56 @@
+// see https://stackoverflow.com/questions/48048957/react-long-press-event
+import { useState, useEffect, useCallback } from 'react'
+
+/**
+ *
+ * see https://stackoverflow.com/questions/48048957/react-long-press-event
+ *
+ * usage:
+ *
+ * import { useLangPress } from '@/hooks'
+ *
+ * const longPress = useLangPress(() => {
+ *   alert("langPress works")
+ * }, 500);
+ *
+ * <Title descExpand={descExpand} {...longPress}>
+ *   ...
+ * </Title>
+ *
+ * @param {*} [callback=() => { }]
+ * @param {number} [ms=500] hold m-sec
+ * @returns
+ */
+const useLongPress = (callback = () => {}, ms = 500) => {
+  const [startLongPress, setStartLongPress] = useState(false)
+
+  useEffect(() => {
+    let timerId
+    if (startLongPress) {
+      timerId = setTimeout(callback, ms)
+    } else {
+      clearTimeout(timerId)
+    }
+
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [callback, ms, startLongPress])
+
+  const start = useCallback(() => {
+    setStartLongPress(true)
+  }, [])
+  const stop = useCallback(() => {
+    setStartLongPress(false)
+  }, [])
+
+  return {
+    onMouseDown: start,
+    onMouseUp: stop,
+    onMouseLeave: stop,
+    onTouchStart: start,
+    onTouchEnd: stop,
+  }
+}
+
+export default useLongPress
