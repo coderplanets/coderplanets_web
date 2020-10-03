9f4c735c24ff1c03f74626f11cd1d2ccedfc8206
mydearxym <mydearxym@qq.com>
2020-10-04 00:07:37 +0800

    refactor(Tabs): auto scroll to active tab on mobile view

--- src/components/Switcher/Tabs/TabItem.js
+++ src/components/Switcher/Tabs/TabItem.js
@@ -33,6 +33,7 @@ const TabItem = ({
 }) => {
   const ref = useRef(null)
   const clickableRef = useRef(null)
+  const activeRef = useRef(null)
 
   // set each tab item width for calc
   useEffect(() => {
@@ -51,6 +52,22 @@ const TabItem = ({
     },
     [onClick, index],
   )
+
+  useEffect(() => {
+    if (item.raw === activeKey && mobileView && !wrapMode) {
+      const curEl = activeRef?.current
+      // 这里的 width 是一个 hack, 每一个 TabItem 会触发设置宽度的
+      // 父元素钩子，导致两次渲染，但是第一次没有调用之前每个 Item 的宽度是 auto
+      // 利用这个特性可以判断真正需要 scroll 到 view 的元素
+      if (curEl && getComputedStyle(curEl).width !== 'auto') {
+        curEl.scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+          // inline: 'center',
+        })
+      }
+    }
+  }, [item, activeKey, mobileView, wrapMode])
 
   return (
     <Wrapper
@@ -74,7 +91,9 @@ const TabItem = ({
             active={item.raw === activeKey}
           />
         )}
-        {isString(item) ? item : item.alias || Trans(item.title)}
+        <div ref={item.raw === activeKey ? activeRef : null}>
+          {isString(item) ? item : item.alias || Trans(item.title)}
+        </div>
       </Label>
 
       {wrapMode && item.raw === activeKey && (
