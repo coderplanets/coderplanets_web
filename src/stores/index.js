f58e8b0da799221243627da2ec6e675bb85882d8
mydearxym <mydearxym@qq.com>
2020-10-05 12:55:43 +0800

    refactor(container): mv AvatarAdder to tool scope

--- src/stores/index.js
+++ src/stores/index.js
@@ -15,7 +15,7 @@ export { default as DoraemonStore } from '@/containers/tool/Doraemon/store'
 export { default as HeaderStore } from '@/containers/unit/Header/store'
 export { default as UpgradePackagesStore } from '@/containers/tool/UpgradePackages/store'
 export { default as MailBoxStore } from '@/containers/MailBox/store'
-export { default as AvatarAdderStore } from '@/containers/AvatarAdder/store'
+export { default as AvatarAdderStore } from '@/containers/tool/AvatarAdder/store'
 
 export { default as UserListerStore } from '@/containers/user/UserLister/store'
 export { default as InformerStore } from '@/containers/tool/Informer/store'
