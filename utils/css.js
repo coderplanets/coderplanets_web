253ae247d02bb7b8d3a0e0d8adee6fb8724d0795
mydearxym <mydearxym@qq.com>
2020-10-26 14:14:40 +0800

    style(article): metrics adjust

--- utils/css.js
+++ utils/css.js
@@ -117,7 +117,7 @@ export const ARTICLE_PAGE_MAX_WIDTH = '1024px'
 // same width as medium.com
 export const ARTICLE_CONTENT_WIDTH = '650px'
 export const ARTICLE_STICKER_WIDTH = '280px'
-export const ARTICLE_CONTENT_OFFSET = '300px'
+export const ARTICLE_CONTENT_OFFSET = '290px'
 
 export const mediaBreakPoints = {
   // mobileS: '320px',
