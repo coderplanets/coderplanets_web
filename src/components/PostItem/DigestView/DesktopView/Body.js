b6667b179a2206592bac3c3de64a5a003ffd274a
mydearxym <mydearxym@qq.com>
2020-11-02 11:43:45 +0800

    refactor(post-item): add preview hint && adjust extra info

--- src/components/PostItem/DigestView/DesktopView/Body.js
+++ src/components/PostItem/DigestView/DesktopView/Body.js
@@ -1,32 +1,44 @@
 import React from 'react'
 import TimeAgo from 'timeago-react'
 
+import { ICON } from '@/config'
 import { cutFrom } from '@/utils'
 
 import {
   Wrapper,
-  CommentsDigest,
   Digest,
-  PublishLabel,
+  Dot,
+  PublishTime,
   Extra,
+  AuthorName,
+  ItemWrapper,
+  ViewsIcon,
+  //
+  PreviewWrapper,
+  PreviewIcon,
+  PreviewText,
 } from '../../styles/digest_view/body'
 
 const Body = ({ item, onPreview }) => {
   return (
     <Wrapper>
       <Extra>
-        {item.author.nickname}
-        {item.copyRight === 'original' ? (
-          <PublishLabel>&nbsp;发布于:</PublishLabel>
-        ) : (
-          <PublishLabel>&nbsp;搬运于:</PublishLabel>
-        )}
-        <TimeAgo datetime={item.insertedAt} locale="zh_CN" /> ⁝ 浏览:{' '}
-        {item.views}
-        <CommentsDigest>⁝ 评论: {item.commentsCount}</CommentsDigest>
+        <AuthorName>{item.author.nickname}</AuthorName>
+        <Dot radius="4px" space="8px" />
+        <PublishTime>
+          <TimeAgo datetime={item.insertedAt} locale="zh_CN" />
+        </PublishTime>
+        <ItemWrapper>
+          <ViewsIcon src={`${ICON}/viewed.svg`} />
+          {item.views}
+        </ItemWrapper>
       </Extra>
       <Digest onClick={() => onPreview(item)}>
         {cutFrom(item.digest, 90)}
+        <PreviewWrapper>
+          <PreviewText>预览</PreviewText>
+          <PreviewIcon src={`${ICON}/shape/arrow-simple.svg`} />
+        </PreviewWrapper>
       </Digest>
     </Wrapper>
   )
