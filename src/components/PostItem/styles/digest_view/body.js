d43a61c4c1420352927fae4c32327aee2580cb44
xieyimian <mydearxym@qq.com>
2020-11-02 11:55:22 +0800

    refactor(PostItem): add preview hint && adjust extra info (#907)
    * fix(typo): footer logo text
    
    * refactor(Footer): email-subscriber style adjust
    
    * refactor(post-item): add preview hint && adjust extra info

--- src/components/PostItem/styles/digest_view/body.js
+++ src/components/PostItem/styles/digest_view/body.js
@@ -2,26 +2,45 @@ import styled from 'styled-components'
 
 import { theme, css } from '@/utils'
 import Img from '@/Img'
+import DotDivider from '@/components/DotDivider'
 
 export const Wrapper = styled.div`
   margin-left: 10px;
   margin-top: -10px;
 `
 export const Extra = styled.li`
-  display: inline;
+  ${css.flex('align-center')};
+  color: ${theme('thread.extraInfo')};
   opacity: 0.7;
   transition: opacity 0.2s;
-  font-size: 13px;
+  font-size: 12px;
+  margin-top: 3px;
+  margin-bottom: 1px;
+`
+export const AuthorName = styled.div`
   color: ${theme('thread.extraInfo')};
+  font-size: 13px;
 `
-export const CommentsDigest = styled.span`
-  display: none;
+export const PublishTime = styled.div`
+  font-size: 12px;
+`
+export const Dot = styled(DotDivider)`
+  background: ${theme('thread.extraInfo')};
+  margin-right: 8px;
+`
+export const ItemWrapper = styled.div`
+  ${css.flex('align-center')};
+  margin-left: 10px;
+`
+export const ViewsIcon = styled(Img)`
+  fill: ${theme('thread.extraInfo')};
+  width: 12px;
+  height: 12px;
+  display: block;
+  opacity: 0.6;
+  margin-right: 3px;
+`
 
-  ${css.media.mobile`
-   display: inline;
-    margin-left: 3px;
-  `};
-`
 export const Digest = styled.li`
   margin-top: 5px;
   color: ${theme('thread.articleDigest')};
@@ -36,31 +55,28 @@ export const Digest = styled.li`
     cursor: pointer;
   }
 
-  ${css.media.mobile`
-    ${css.cutFrom('250px')};
-  `};
-
   transition: color 0.2s;
 `
-export const CommentWrapper = styled.div`
-  ${css.flex('align-center')};
-  align-self: flex-start;
+export const PreviewWrapper = styled.div`
+  display: inline-flex;
+  opacity: 0;
+  margin-left: 8px;
+  align-items: center;
+
+  ${Wrapper}:hover & {
+    opacity: 0.6;
+  }
+  transition: opacity 0.25s;
 `
-export const CommentIcon = styled(Img)`
-  fill: ${theme('thread.articleDigest')};
-  width: 14px;
+export const PreviewIcon = styled(Img)`
+  fill: ${theme('thread.extraInfo')};
   height: 14px;
-  margin-right: 4px;
+  width: 14px;
   display: block;
+  transform: rotate(180deg);
 `
-export const CommentNum = styled.div`
-  color: ${theme('thread.articleTitle')};
-  font-size: 0.9rem;
-`
-export const PublishLabel = styled.span`
-  display: inline;
-  ${css.media.mobile`
-    display: none;
-    margin-left: 3px;
-  `};
+export const PreviewText = styled.span`
+  color: ${theme('thread.extraInfo')};
+  font-size: 12px;
+  margin-right: 5px;
 `
