01e6306109652b3bd5e8c0d1d7fe63e8ce330d27
mydearxym <mydearxym@qq.com>
2020-11-03 10:15:02 +0800

    refactor(lazyimage): show Fallback unless load done && ssr by default

--- src/components/Img/LazyLoadImg.js
+++ src/components/Img/LazyLoadImg.js
@@ -2,15 +2,7 @@ import React, { useState } from 'react'
 import T from 'prop-types'
 
 import { LazyLoadImage } from 'react-lazy-load-image-component'
-
-// NOTE: the blur effect css is located in thirdPartyOverWrite.js
-
-// NOTE: do not use fallback directly, it will block the image display
-// seems the LazyLoadImage's issue
-const PlaceHolder = ({ child }) => {
-  // <div style={{ height: '20px', width: '20px', background: '#139C9E' }}>
-  return <div>{child}</div>
-}
+import { Wrapper, FallbackWrapper } from './styles/lazy_load_image'
 
 /**
  * lazy load images like .jpg .jpeg .png  etc
@@ -25,34 +17,24 @@ const LazyLoadImg = ({
   scrollPosition,
   visibleByDefault,
 }) => {
-  const [imgError, setImgError] = useState(false)
+  const [imgLoaded, setImgLoaded] = useState(false)
 
   return (
-    <React.Fragment>
-      {imgError && fallback ? (
-        <React.Fragment>{fallback}</React.Fragment>
-      ) : (
-        <LazyLoadImage
-          className={className}
-          src={src}
-          alt={alt}
-          placeholder={<PlaceHolder child={fallback} />}
-          effect="blur"
-          scrollPosition={scrollPosition}
-          visibleByDefault={visibleByDefault}
-          beforeLoad={() => {
-            const picture = new Image()
-            picture.src = src
-            picture.onload = () => {
-              if (picture.naturalWidth + picture.naturalHeight === 0) {
-                setImgError(true)
-              }
-            }
-            picture.onerror = () => setImgError(true)
-          }}
-        />
-      )}
-    </React.Fragment>
+    <Wrapper>
+      <FallbackWrapper>
+        {!imgLoaded && <React.Fragment>{fallback}</React.Fragment>}
+      </FallbackWrapper>
+      <LazyLoadImage
+        className={className}
+        src={src}
+        alt={alt}
+        // placeholder={<PlaceHolder child={fallback} />}
+        effect="blur"
+        scrollPosition={scrollPosition}
+        visibleByDefault={visibleByDefault}
+        afterLoad={() => setImgLoaded(true)}
+      />
+    </Wrapper>
   )
 }
 
