cbb3f4c4e938b76010cd16023fbb26b662cc351f
mydearxym <mydearxym@qq.com>
2020-11-15 15:57:02 +0800

    style(works): adjust BestTab style

--- src/containers/content/WorksContent/List/BestTab.js
+++ src/containers/content/WorksContent/List/BestTab.js
@@ -1,17 +1,12 @@
 import React from 'react'
 
-import { ICON } from '@/config'
-import { Wrapper, ItemWrapper, Icon, Text } from '../styles/list/best_tab'
+import { Wrapper, ItemWrapper, Text } from '../styles/list/best_tab'
 
 const BestTab = ({ items, activeKey }) => {
   return (
     <Wrapper>
       {items.map((item) => (
-        <ItemWrapper key={item.key}>
-          <Icon
-            src={`${ICON}/shape/crown.svg`}
-            active={item.raw === activeKey}
-          />
+        <ItemWrapper key={item.key} active={item.raw === activeKey}>
           <Text active={item.raw === activeKey}>{item.title}</Text>
         </ItemWrapper>
       ))}
