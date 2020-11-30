a29807521940420fc9b46bca1cbc093d996a0790
xieyimian <mydearxym@qq.com>
2020-11-30 21:43:22 +0800

    refactor(article-editor): basic setting panel (#967)

--- src/components/SectionLabel/styles/index.js
+++ src/components/SectionLabel/styles/index.js
@@ -10,7 +10,8 @@ export const Label = styled.div`
   ${css.flex()};
 `
 export const Title = styled.div`
-  color: ${theme('tabs.headerActive')};
+  /* color: ${theme('tabs.headerActive')}; */
+  color: ${theme('thread.articleDigest')};
   font-size: 0.9rem;
   flex-grow: 1;
 `
@@ -25,13 +26,13 @@ export const Divider = styled.div`
   border-bottom: 1px solid;
   border-color: ${theme('banner.desc')};
   margin-top: 12px;
-  margin-bottom: 18px;
-  width: 95%;
+  margin-bottom: 15px;
+  width: 100%;
   opacity: 0.3;
 `
-
 export const LabelIcon = styled(Img)`
-  fill: ${theme('tabs.headerActive')};
+  fill: ${theme('thread.articleDigest')};
+  /* fill: ${theme('tabs.headerActive')}; */
   ${css.size(18)};
   margin-right: 5px;
   margin-left: 5px;
@@ -39,7 +40,9 @@ export const LabelIcon = styled(Img)`
 `
 
 export const Desc = styled.div`
-  color: ${theme('banner.desc')};
+  color: ${theme('thread.articleDigest')};
+  /* color: ${theme('banner.desc')}; */
+  opacity: 0.8;
   font-size: 14px;
   margin-bottom: 18px;
   margin-left: 3px;
--- src/containers/editor/ArticleEditor/Settings/index.js
+++ src/containers/editor/ArticleEditor/Settings/index.js
@@ -1,23 +1,73 @@
 import React from 'react'
 
-import { ArrowButton, Button } from '@/components/Buttons'
+import { ICON } from '@/config'
 
-import { Wrapper, ContentWrapper, Footer } from '../styles/settings'
+import { ArrowButton, Button } from '@/components/Buttons'
+import SectionLabel from '@/components/SectionLabel'
+import Checker from '@/components/Checker'
+import Select from '@/components/Select'
+
+import { Wrapper, ContentWrapper, CheckItem, Footer } from '../styles/settings'
 import { previousStep, nextStep } from '../logic'
+
+const communityOptions = [
+  { value: 'javascript', label: 'javascript', desc: '前端 javascript 相关' },
+  { value: 'elixir', label: 'elixir', desc: 'elixir 编程语言' },
+]
+
+const tagOptions = [
+  { value: '技术', label: 'Web', desc: '网站，浏览器扩展等' },
+  { value: '职场', label: 'iOS', desc: 'App, 平板应用等' },
+  { value: '分享', label: 'Android', desc: 'App, 平板应用等' },
+  { value: '问答', label: 'Mac' },
+  { value: '视频', label: 'Windows' },
+]
 
 const Settings = () => {
   return (
     <Wrapper>
       <ContentWrapper>
-        <h3>社区设置</h3>
+        <SectionLabel
+          title="发布到社区"
+          desc="帖子所属的社区，发布后只有管理员能移动或同步到其他社区。"
+          iconSrc={`${ICON}/shape/planet.svg`}
+        />
+        <Select options={communityOptions} placeholder="请选择社区" />
         <br />
-        <h3>标签设置</h3>
+        <SectionLabel
+          title="设置标签"
+          desc="帖子相关标签，发布后只有管理员能重新设置或更新标签。"
+          iconSrc={`${ICON}/article/tag.svg`}
+        />
+        <Select options={tagOptions} placeholder="请选择标签" isMulti />
         <br />
-        <h3>文章设置</h3>
-        <br />
-        <h4>是否允许评论</h4>
-        <h4>设置打赏</h4>
-        <h4>设置公众号</h4>
+        <SectionLabel title="文章设置" iconSrc={`${ICON}/shape/setting.svg`} />
+        <CheckItem>
+          <Checker
+            checked
+            // onChange={(checked) => updateWorks('isOSS', checked)}
+            // size="small"
+          >
+            允许评论
+          </Checker>
+        </CheckItem>
+        <CheckItem>
+          <Checker
+            checked={false}
+            // onChange={(checked) => updateWorks('isOSS', checked)}
+            // size="small"
+          >
+            有新评论使用邮件通知我
+          </Checker>
+        </CheckItem>
+        <CheckItem>
+          <Checker
+            checked={false}
+            // onChange={(checked) => updateWorks('isOSS', checked)}
+          >
+            推荐自己的其他帖子
+          </Checker>
+        </CheckItem>
       </ContentWrapper>
       <Footer>
         <ArrowButton direction="left" onClick={previousStep} dimWhenIdle>
