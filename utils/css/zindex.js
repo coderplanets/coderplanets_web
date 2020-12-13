41244b093c987c67ddc9528757a1ac5f825c0065
mydearxym <mydearxym@qq.com>
2020-12-13 22:16:42 +0800

    refactor(css): re-org based on function

---
+++ utils/css/zindex.js
@@ -0,0 +1,17 @@
+const zIndex = {
+  img: 2,
+  header: 3,
+  popover: 2000,
+  modalOverlay: 2500,
+  drawerOverlay: 2000,
+  drawer: 2001,
+  modeLine: 2002,
+  doraemonOverlay: 2600,
+  doraemon: 2601,
+  modalCloseBtn: 2000,
+
+  //
+  sidebar: 1999,
+}
+
+export default zIndex
