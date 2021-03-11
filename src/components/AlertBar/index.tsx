3e94ba991ae7e8a69cb923161365e9bb8db82f81
mydearxym <mydearxym@qq.com>
2021-03-11 16:30:01 +0800

    chore(ts-workflow): test/*js -> ts && resolve wanings

--- src/components/AlertBar/index.tsx
+++ src/components/AlertBar/index.tsx
@@ -5,7 +5,6 @@
  */
 
 import React from 'react'
-import T from 'prop-types'
 
 import { buildLog } from '@/utils'
 
@@ -14,14 +13,12 @@ import { Wrapper } from './styles'
 /* eslint-disable-next-line */
 const log = buildLog('c:AlertBar:index')
 
-const AlertBar = ({ children }) => {
+type IProps = {
+  children: React.ReactNode
+}
+
+const AlertBar: React.FC<IProps> = ({ children }) => {
   return <Wrapper testid="alertBar">{children}</Wrapper>
 }
-
-AlertBar.propTypes = {
-  children: T.oneOfType([T.string, T.node]).isRequired,
-}
-
-AlertBar.defaultProps = {}
 
 export default React.memo(AlertBar)
