d996f8282ff74be353df3c6d2ef7cbe958af902c
mydearxym <mydearxym@qq.com>
2021-04-26 12:45:27 +0800

    chore(abuse-report): build error

--- src/components/ArticleActionsPanel/styles/index.ts
+++ src/components/ArticleActionsPanel/styles/index.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
-import { css } from '@/utils'
+import { theme, css } from '@/utils'
+import Img from '@/Img'
 
 export const Wrapper = styled.div`
   ${css.flexColumn()};
@@ -8,5 +9,30 @@ export const Wrapper = styled.div`
   padding-left: 10px;
   padding-bottom: 0;
 `
+export const Option = styled.div<{ red: boolean }>`
+  ${css.flex('align-center')};
+  color: ${theme('banner.desc')};
+  margin-bottom: 8px;
 
-export const holder = 1
+  &:hover {
+    color: ${({ red }) => (red ? 'tomato' : theme('banner.title'))};
+
+    cursor: pointer;
+  }
+`
+type TOptionIcon = {
+  red: boolean
+  reverse: boolean
+}
+export const OptionIcon = styled(Img)<TOptionIcon>`
+  fill: ${theme('banner.desc')};
+  ${css.size(18)};
+  margin-right: 6px;
+  transform: ${({ reverse }) => (reverse ? 'rotate(180deg)' : '')};
+  ${Option}:hover & {
+    fill: ${({ red }) => (red ? 'tomato' : theme('banner.title'))};
+  }
+`
+export const OptionTitle = styled.div`
+  font-size: 0.9rem;
+`
