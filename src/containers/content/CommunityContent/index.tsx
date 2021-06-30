0cffeea6c5b5d6e249d42a6c69d319cd9e4e8467
mydearxym <mydearxym@qq.com>
2021-06-30 23:46:28 +0800

    feat: add subed communities demo list

--- src/containers/content/CommunityContent/index.tsx
+++ src/containers/content/CommunityContent/index.tsx
@@ -6,7 +6,6 @@
 
 import { FC } from 'react'
 
-import { ICON_BASE } from '@/config'
 import { ROUTE, C11N } from '@/constant'
 import { useDevice } from '@/hooks'
 import { pluggedIn, buildLog } from '@/utils'
@@ -18,16 +17,10 @@ import ReposThread from '@/containers/thread/ReposThread'
 import JobsThread from '@/containers/thread/JobsThread'
 import UsersThread from '@/containers/thread/UsersThread'
 
+import SubscribedList from './SubscribedList'
 import type { TStore } from './store'
-import {
-  Wrapper,
-  InnerWrapper,
-  TmpSubedBox,
-  SubItem,
-  SubIcon,
-  SubTitle,
-  ItemDivider,
-} from './styles'
+
+import { Wrapper, InnerWrapper } from './styles'
 import { useInit } from './logic'
 
 /* eslint-disable-next-line */
@@ -66,42 +59,17 @@ const CommunityContentContainer: FC<TProps> = ({ communityContent: store }) => {
     accountInfo: {
       customization: { bannerLayout },
     },
+    subscribedCommunitiesData,
   } = store
 
   const isClassicLayout = !isMobile && bannerLayout === C11N.DIGEST
 
+  // console.log('subscribedCommunitiesData ->> ', subscribedCommunitiesData)
+
   return (
     <Wrapper testid="community-content">
       <InnerWrapper isClassicLayout={isClassicLayout}>
-        <TmpSubedBox>
-          <SubTitle>我的订阅</SubTitle>
-          <ItemDivider />
-          <SubItem>首页</SubItem>
-          <SubItem>
-            <SubIcon src={`${ICON_BASE}/pl/javascript.svg`} />
-            JavaScript
-          </SubItem>
-          <SubItem>
-            <SubIcon src={`${ICON_BASE}/pl/elixir.svg`} />
-            Elixir
-          </SubItem>
-          <SubItem>
-            <SubIcon src={`${ICON_BASE}/pl/clojure.svg`} />
-            神奇女侠
-          </SubItem>
-          <SubItem>
-            <SubIcon src={`${ICON_BASE}/pl/javascript.svg`} />
-            JavaScript
-          </SubItem>
-          <SubItem>
-            <SubIcon src={`${ICON_BASE}/pl/elixir.svg`} />
-            Elixir
-          </SubItem>
-          <SubItem>
-            <SubIcon src={`${ICON_BASE}/pl/clojure.svg`} />
-            神奇女侠
-          </SubItem>
-        </TmpSubedBox>
+        <SubscribedList communities={subscribedCommunitiesData} />
         <ComunityContent curRoute={curRoute} />
         <CommunityDigest />
       </InnerWrapper>
--- src/containers/content/CommunityContent/styles/index.ts
+++ src/containers/content/CommunityContent/styles/index.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 
-import Img from '@/Img'
 import type { TTestable } from '@/spec'
 import { theme, css, WIDTH, pixelAdd } from '@/utils'
 
@@ -43,39 +42,4 @@ export const InnerWrapper = styled.div<{ isClassicLayout: boolean }>`
     margin: 0 3%;
     padding-top: 0;
   `};
-`
-
-export const TmpSubedBox = styled.div`
-  width: 200px;
-  margin-top: 10px;
-  margin-right: 20px;
-  height: 400px;
-  color: #5b7b81;
-  padding-top: 10px;
-  padding-left: 10px;
-`
-export const SubTitle = styled.div`
-  font-size: 15px;
-`
-export const ItemDivider = styled.div`
-  width: 80px;
-  height: 1px;
-  background: #004757;
-  margin-top: 8px;
-  margin-bottom: 10px;
-`
-export const SubItem = styled.div`
-  ${css.flex('align-center')};
-  font-size: 14px;
-  margin-bottom: 15px;
-
-  &:hover {
-    cursor: pointer;
-  }
-`
-export const SubIcon = styled(Img)`
-  ${css.size(18)};
-  border-radius: 5px;
-  margin-right: 7px;
-  filter: saturate(0.5);
 `
