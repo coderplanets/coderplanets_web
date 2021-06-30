810b467d88ceb430eadf8b0418120d1da5ece162
mydearxym <mydearxym@qq.com>
2021-06-30 23:05:04 +0800

    chore: add template for PublishButton

--- src/components/Buttons/PublishButton.tsx
+++ src/components/Buttons/PublishButton.tsx
@@ -18,33 +18,58 @@ import { Wrapper } from './styles/publish_button'
 /* eslint-disable-next-line */
 const log = buildLog('c:PublishButton:index')
 
+const getOptions = (thread) => {
+  switch (thread) {
+    case THREAD.JOB: {
+      return [
+        {
+          key: 'publish',
+          icon: `${ICON}/edit/publish-write.svg`,
+          title: '我要招人',
+          desc: '发布后会第一次出现在相应版块首页。',
+        },
+        {
+          key: 'link',
+          link: 'https://newpage',
+          icon: `${ICON}/article/report.svg`,
+          title: '发布须知',
+          desc:
+            '请确保你发布的内容符合社区的基本价值观和规范，否则会被限流或直接删除。',
+        },
+      ]
+    }
+    default: {
+      return [
+        {
+          key: 'publish',
+          icon: `${ICON}/edit/publish-write.svg`,
+          title: '发布帖子',
+          desc: '发布后会第一次出现在相应版块首页。',
+        },
+        {
+          key: 'link',
+          link: 'https://newpage',
+          icon: `${ICON}/article/report.svg`,
+          title: '发布须知',
+          desc:
+            '请确保你发布的内容符合社区的基本价值观和规范，否则会被限流或直接删除。',
+        },
+      ]
+    }
+  }
+}
+
 type TProps = {
   thread?: TThread
   onCreate?: () => void
 }
 
 const PublishButton: FC<TProps> = ({ thread = THREAD.POST, onCreate }) => {
-  // TODO: use thread template
   return (
     <Wrapper>
       <DropdownButton
         placement="bottom-end"
-        options={[
-          {
-            key: 'publish',
-            icon: `${ICON}/edit/publish-write.svg`,
-            title: '发布帖子',
-            desc: '发布后会第一次出现在相应版块首页。',
-          },
-          {
-            key: 'link',
-            link: 'https://newpage',
-            icon: `${ICON}/article/report.svg`,
-            title: '发布须知',
-            desc:
-              '请确保你发布的内容符合社区的基本价值观和规范，否则会被限流或直接删除。',
-          },
-        ]}
+        options={getOptions(thread)}
         panelMinWidth="210px"
         onClick={(key) => {
           if (key === 'publish') onCreate()
