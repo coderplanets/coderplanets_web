172d8cb3b9ccdb406879cfa521fb25fa3edc630a
mydearxym <mydearxym@qq.com>
2021-07-31 10:27:36 +0800

    chore: rm sentry wip

--- next.config.js
+++ next.config.js
@@ -44,28 +44,6 @@ const nextConfig = {
       config.resolve.alias['@sentry/node'] = '@sentry/browser'
     }
 
-    // When all the Sentry configuration env variables are available/configured
-    // The Sentry webpack plugin gets pushed to the webpack plugins to build
-    // and upload the source maps to sentry.
-    // This is an alternative to manually uploading the source maps
-    // Note: This is disabled in development mode.
-    if (
-      SENTRY_DSN &&
-      SENTRY_ORG &&
-      SENTRY_PROJECT &&
-      SENTRY_AUTH_TOKEN &&
-      NODE_ENV === 'production'
-    ) {
-      config.plugins.push(
-        new SentryWebpackPlugin({
-          include: '.next',
-          ignore: ['node_modules'],
-          urlPrefix: '~/_next',
-          release: buildId,
-        }),
-      )
-    }
-
     return config
   },
 }
