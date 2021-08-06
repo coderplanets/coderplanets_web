6d19e1fe3f5f7e570f7e23f0fac6d86e45bd81cd
mydearxym <mydearxym@qq.com>
2021-08-06 13:32:06 +0800

    chore: drop offline

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const webpack = require('webpack')
 // next-plugins
 const withPlugins = require('next-compose-plugins')
 const withSourceMaps = require('@zeit/next-source-maps')()
-const withOffline = require('next-offline')
+// const withOffline = require('next-offline')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-const offlineConfig = require('./config/next_offline')
+// const offlineConfig = require('./config/next_offline')
 
 // next-plugins end
 
@@ -26,6 +26,7 @@ const nextConfig = {
 }
 
 module.exports = withPlugins(
-  [withBundleAnalyzer, withSourceMaps, [withOffline, offlineConfig]],
+  // [withBundleAnalyzer, withSourceMaps, [withOffline, offlineConfig]],
+  [withBundleAnalyzer, withSourceMaps],
   nextConfig,
 )
