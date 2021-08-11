41dd4b84897c6b05250ef9cdff0c1cb47436c6c6
mydearxym <mydearxym@qq.com>
2021-08-11 16:59:21 +0800

    chore: tags adjust

--- src/components/ArticleMenu/index.tsx
+++ src/components/ArticleMenu/index.tsx
@@ -5,17 +5,17 @@
 import { FC, memo } from 'react'
 import dynamic from 'next/dynamic'
 
-import RealArticleMenu from './RealArticleMenu'
+// import RealArticleMenu from './RealArticleMenu'
 
 type TProps = {
   testid?: string
 }
 
-// const RealArticleMenu = dynamic(() => import('./RealArticleMenu'), {
-//   ssr: false,
-// })
+const RealArticleMenu = dynamic(() => import('./RealArticleMenu'), {
+  ssr: false,
+})
 
-const ArticleMenu: FC<TProps> = ({ testid = 'archived-sign' }) => {
+const ArticleMenu: FC<TProps> = ({ testid = 'artile-menu' }) => {
   return <RealArticleMenu testid={testid} />
 }
 
