a0791d7e5212f169ef19cf02c8bb5fdea13aed91
mydearxym <mydearxym@qq.com>
2021-09-27 14:22:13 +0800

    chore: pack issue

--- deploy/production/loader.sh
+++ deploy/production/loader.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cd /root/web/
+ls -llh ./
 
 # make build.prod
-pm2-runtime start --max-memory-restart 1G npm --name "coderplanets_web" -- run serve.prod &
+# pm2-runtime start --max-memory-restart 1G npm --name "coderplanets_web" -- run serve.prod &
+npm run serve.prod &
 
 while true
 do
