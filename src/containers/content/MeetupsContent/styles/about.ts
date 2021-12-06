bbf446292696a59b08aeb062feb339b0b6360125
mydearxym <mydearxym@qq.com>
2021-12-06 11:43:02 +0800

    chore(meetups): adjust holder

--- src/containers/content/MeetupsContent/styles/about.ts
+++ src/containers/content/MeetupsContent/styles/about.ts
@@ -7,7 +7,7 @@ import css from '@/utils/css'
 export const Wrapper = styled.div`
   width: 100%;
   color: ${theme('thread.articleDigest')};
-  font-size: 15px;
+  font-size: 14px;
   margin-top: 50px;
   border-top: 1px solid #104352;
   padding-top: 20px;
