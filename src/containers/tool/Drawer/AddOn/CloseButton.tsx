bafce48ff49afbd2e5e8c9a0382f76c708c7e920
mydearxym <mydearxym@qq.com>
2021-12-07 17:11:37 +0800

    style(drawer): addon icon size

--- src/containers/tool/Drawer/AddOn/CloseButton.tsx
+++ src/containers/tool/Drawer/AddOn/CloseButton.tsx
@@ -4,7 +4,7 @@ import IconButton from '@/widgets/Buttons/IconButton'
 import { closeDrawer } from '../logic'
 
 const CloseButton: FC = () => {
-  return <IconButton icon="close" onClick={closeDrawer} size={26} dimWhenIdle />
+  return <IconButton icon="close" onClick={closeDrawer} size={21} dimWhenIdle />
 }
 
 export default memo(CloseButton)
