1a24899d9542b2347e8d9d51179c3391d71f8497
mydearxym <mydearxym@qq.com>
2021-12-14 12:30:43 +0800

    feat(user): add simple intro banner on mobile

---
+++ src/containers/content/UserContent/styles/mobile_banner.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+import type { TMetric } from '@/spec'
+
+import { theme } from '@/utils/themes'
+import css from '@/utils/css'
+
+import Img from '@/Img'
+
+export const Wrapper = styled.div`
+  ${css.flex('align-center')};
+  width: 100%;
+  padding: 20px;
+`
+export const Avatar = styled(Img)`
+  ${css.size(60)};
+  border-radius: 42%;
+`
+export const Intro = styled.div`
+  margin-left: 18px;
+`
+export const NickName = styled.div`
+  font-size: 16px;
+  font-weight: bold;
+  color: ${theme('thread.articleTitle')};
+`
+export const Bio = styled.div`
+  font-size: 13px;
+  color: ${theme('thread.articleDigest')};
+  position: relative;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-box-orient: vertical;
+`
