4dc18998727571138ece6328bdd4d932ceabdb94
mydearxym <mydearxym@qq.com>
2021-12-16 12:51:07 +0800

    chore(deploy): bump version to v2.0.80

--- src/containers/tool/JoinModal/constant.tsx
+++ src/containers/tool/JoinModal/constant.tsx
@@ -3,29 +3,29 @@ import { ICON_BASE } from '@/config'
 export const GROUPS = {
   IN: {
     title: 'CP 内测群',
-    QR: `${ICON_BASE}/cps_wechat_group.png`,
+    QR: `${ICON_BASE}/cp_wx_qr_eurh.png`,
   },
   // V: {
   //   title: 'CP 志愿者',
-  //   QR: `${ICON_BASE}/cps_wechat_group.png`,
+  //   QR: `${ICON_BASE}/cp_wx_qr_eurh.png`,
   // },
   // 雷达站
   R: {
     title: 'CP 雷达站',
-    QR: `${ICON_BASE}/cps_wechat_group.png`,
+    QR: `${ICON_BASE}/cp_wx_qr_eurh.png`,
   },
   // 创作者群
   W: {
     title: 'CP Makers',
-    QR: `${ICON_BASE}/cps_wechat_group.png`,
+    QR: `${ICON_BASE}/cp_wx_qr_eurh.png`,
   },
   G: {
     title: 'CP 酷导航',
-    QR: `${ICON_BASE}/cps_wechat_group.png`,
+    QR: `${ICON_BASE}/cp_wx_qr_eurh.png`,
   },
   D: {
     title: 'CP 来一杯',
-    QR: `${ICON_BASE}/cps_wechat_group.png`,
+    QR: `${ICON_BASE}/cp_wx_qr_eurh.png`,
   },
 }
 
