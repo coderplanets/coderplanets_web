8d270c5a0d95991c99fd91d114d7bb54739ff02b
mydearxym <mydearxym@qq.com>
2021-12-31 21:18:15 +0800

    refactor(route): re-org explore as home page

---
+++ src/pages/hot.tsx
@@ -0,0 +1,110 @@
+import { Provider } from 'mobx-react'
+import { GetStaticProps } from 'next'
+import { merge, toLower } from 'ramda'
+
+// import { PAGE_SIZE } from '@/config'
+import { HCN, THREAD, METRIC, ROUTE } from '@/constant'
+import { useStore } from '@/stores/init'
+
+import {
+  isArticleThread,
+  ssrPagedArticleSchema,
+  isrPagedArticlesFilter,
+  ssrParseArticleThread,
+  communitySEO,
+  singular,
+  makeGQClient,
+} from '@/utils'
+
+import GlobalLayout from '@/containers/layout/GlobalLayout'
+import CommunityContent from '@/containers/content/CommunityContent'
+
+import { P } from '@/schemas'
+
+const loader = async () => {
+  const gqClient = makeGQClient('')
+
+  // 线上环境会直接跳过 index 到这里，有待排查。。
+  const community = HCN
+  const thread = THREAD.POST
+
+  // query data
+  const curCommunity = gqClient.request(P.community, {
+    raw: community,
+    userHasLogin: false,
+  })
+
+  const pagedArticleTags = gqClient.request(P.pagedArticleTags, {
+    filter: { communityRaw: community, thread: singular(thread, 'upperCase') },
+  })
+
+  const filter = isrPagedArticlesFilter({})
+
+  const pagedArticles = isArticleThread(thread)
+    ? gqClient.request(ssrPagedArticleSchema(thread), filter)
+    : {}
+
+  return {
+    filter,
+    ...(await pagedArticleTags),
+    ...(await curCommunity),
+    ...(await pagedArticles),
+  }
+}
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   return { paths: [], fallback: true }
+// }
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  console.log('hot params: ', ctx)
+
+  const thread = THREAD.POST
+  const resp = await loader()
+
+  const { filter, community, pagedArticleTags } = resp
+  // console.log('iii got resp: ', resp)
+  const articleThread = ssrParseArticleThread(resp, thread, filter)
+
+  const initProps = merge(
+    {
+      // ...ssrBaseStates(resp),
+      route: {
+        communityPath: community.raw,
+        mainPath: ROUTE.HOT,
+        subPath: thread === THREAD.POST ? '' : thread,
+        thread,
+      },
+      tagsBar: {
+        tags: pagedArticleTags.entries,
+      },
+      viewing: {
+        community,
+        activeThread: toLower(thread),
+      },
+    },
+    articleThread,
+  )
+
+  return { props: { errorCode: null, ...initProps }, revalidate: 10 }
+}
+
+const CommunityPage = (props) => {
+  const store = useStore(props)
+
+  const { viewing } = store
+  const { community, activeThread } = viewing
+
+  return (
+    <Provider store={store}>
+      <GlobalLayout
+        metric={METRIC.COMMUNITY}
+        seoConfig={communitySEO(community, activeThread)}
+      >
+        <CommunityContent />
+      </GlobalLayout>
+    </Provider>
+  )
+}
+
+export default CommunityPage
