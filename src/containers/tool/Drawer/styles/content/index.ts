eeca92e160a0d1672dd77b1515ee4317998ed0d9
mydearxym <mydearxym@qq.com>
2022-02-13 23:44:27 +0800

    style(drawer): rm scroll style on windows

--- src/containers/tool/Drawer/styles/content/index.ts
+++ src/containers/tool/Drawer/styles/content/index.ts
@@ -6,7 +6,6 @@ export const Wrapper = styled.div`
   width: 100%;
   /* NOTE:  those property must exist otherwise custom scroller will not work*/
   height: 100%;
-  overflow-y: scroll;
 
   /* 30px is the modeLine height */
   ${css.media.mobile`
