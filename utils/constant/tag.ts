3cc065bac1f17304af7a9fe244195063c1c8e8b6
mydearxym <mydearxym@qq.com>
2022-04-27 16:49:51 +0800

    refactor(type): use Record for json constant

--- utils/constant/tag.ts
+++ utils/constant/tag.ts
@@ -1,8 +1,8 @@
 import type { TTagMode } from '@/spec'
 
 const TAG_MODE = {
-  DEFAULT: 'default' as TTagMode,
-  LABEL: 'label' as TTagMode,
-}
+  DEFAULT: 'default',
+  LABEL: 'label',
+} as Record<Uppercase<TTagMode>, TTagMode>
 
 export default TAG_MODE
