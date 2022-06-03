14b46d6f55680d4ea89510fced2aa49c17a4f2dc
mydearxym <mydearxym@qq.com>
2022-06-03 21:26:09 +0800

    refactor(dashboard-post-layout): enhance post attrs

--- src/widgets/DashboardDesc/PostListExample.tsx
+++ src/widgets/DashboardDesc/PostListExample.tsx
@@ -10,6 +10,7 @@ import { POST_LAYOUT, ARTICLE_CAT, ARTICLE_STATE } from '@/constant'
 
 import { buildLog } from '@/utils/logger'
 import { mockUsers } from '@/utils/mock'
+import { getRandomInt } from '@/utils/helper'
 
 import NoticeBar from '@/widgets/NoticeBar'
 import PostItem from '@/widgets/PostItem'
@@ -21,11 +22,12 @@ const log = buildLog('c:DashboardDesc:index')
 
 const demoPost = {
   id: '0',
-  title: '这是一篇测试帖子',
-  digest: '这是玉兰信息',
-  views: 200,
-  upvotesCount: 10,
-  commentsCount: 15,
+  title: '帖子是否可以像评论一样开放表情功能?',
+  digest:
+    '帖子以及更新日志等文章类除了常见的 Upvote 以外，是否可以支持表情功能，像目前评论一样，但是具体表情有区别',
+  views: getRandomInt(50, 399),
+  upvotesCount: getRandomInt(50, 99),
+  commentsCount: getRandomInt(50, 99),
   articleTags: [],
   category: ARTICLE_CAT.FEATURE,
   state: ARTICLE_STATE.TODO,
@@ -41,6 +43,29 @@ const demoPost = {
 }
 
 const PostListExample: FC = () => {
+  const article1 = {
+    ...demoPost,
+    title: '看板板块在手机端显示异常',
+    digest:
+      '手机端帖子标题换行有错位问题，看起来不够直观，建议参考电商常见的搜索热榜等展示形式。',
+    category: ARTICLE_CAT.BUG,
+    state: ARTICLE_STATE.WIP,
+    views: getRandomInt(50, 399),
+    upvotesCount: getRandomInt(50, 99),
+    commentsCount: getRandomInt(50, 99),
+  }
+
+  const article2 = {
+    ...demoPost,
+    title: '用户页能否看到发帖历史？',
+    digest: '用户主页能否展示在社区内的历史发帖或评论，提高用户参与的热情？',
+    category: ARTICLE_CAT.QUESTION,
+    state: ARTICLE_STATE.RESOLVE,
+    views: getRandomInt(50, 399),
+    upvotesCount: getRandomInt(50, 99),
+    commentsCount: getRandomInt(50, 99),
+  }
+
   return (
     <Wrapper>
       <NoticeBar
@@ -52,8 +77,8 @@ const PostListExample: FC = () => {
       <Title>布局 A</Title>
       <Desc>侧重展示标题与参与 Upvotes 的用户</Desc>
       <PostItem article={demoPost} c11n={{}} curCommunity={{ raw: 'demo' }} />
-      <PostItem article={demoPost} c11n={{}} curCommunity={{ raw: 'demo' }} />
-      <PostItem article={demoPost} c11n={{}} curCommunity={{ raw: 'demo' }} />
+      <PostItem article={article1} c11n={{}} curCommunity={{ raw: 'demo' }} />
+      <PostItem article={article2} c11n={{}} curCommunity={{ raw: 'demo' }} />
 
       <DividerLine />
       <Title>布局 B</Title>
@@ -66,13 +91,13 @@ const PostListExample: FC = () => {
         layout={POST_LAYOUT.COMMENT_FIRST}
       />
       <PostItem
-        article={demoPost}
+        article={article1}
         c11n={{}}
         curCommunity={{ raw: 'demo' }}
         layout={POST_LAYOUT.COMMENT_FIRST}
       />
       <PostItem
-        article={demoPost}
+        article={article2}
         c11n={{}}
         curCommunity={{ raw: 'demo' }}
         layout={POST_LAYOUT.COMMENT_FIRST}
