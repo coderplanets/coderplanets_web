e6a517ab25a90fef3a54f299a64dc4c67fbf1406
mydearxym <mydearxym@qq.com>
2022-09-11 21:33:56 +0800

    feat: sync appVserion from pakcage.json

--- global.d.ts
+++ global.d.ts
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { IziToast } from 'iziToast'
 
-declare global {
-  interface IWindow extends Window {
-    iziToast?: IziToast
-    appVersion?: string
-    /**
-     * used for check platform hook
-     */
-    chrome?: any
-    safari?: any
-    StyleMedia?: any
-    HTMLElement?: any
-  }
+interface IWindow extends Window {
+  iziToast?: IziToast
+  appVersion?: string
+  /**
+   * used for check platform hook
+   */
+  chrome?: any
+  safari?: any
+  StyleMedia?: any
+  HTMLElement?: any
+}
 
+declare global {
   type TWindow = IWindow | null
 }
